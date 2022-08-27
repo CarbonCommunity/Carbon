233643d3856fd9d245782f2746a13d39acb8ab56
raul <raulssorban@gmail.com>
2022-08-28 00:43:37 +0200

    Fixed console commands

--- Rexide.Core/Rexide/Oxide/Hooks/ConsoleSystem_Run.cs
+++ Rexide.Core/Rexide/Oxide/Hooks/ConsoleSystem_Run.cs
@@ -2,36 +2,41 @@
 using Facepunch;
 using Harmony;
 using System;
+using System.CodeDom;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 [HarmonyPatch ( typeof ( ConsoleSystem ), "Run" )]
 public class ConsoleSystem_Run
 {
+    internal static List<string> _argumentBuffer = new List<string> ( 10 );
+
     public static void Prefix ( ConsoleSystem.Option options, string strCommand, object [] args, ref string __result )
     {
-        Rexide.Log ( $"ConsoleSystem_Run: {strCommand} {string.Join( " ", args )}" );
 
-        var args2 = Facepunch.Pool.GetList<string>();
-        foreach(var arg in args) args.Add( arg );
-        var args3 = args2.ToArray ();
+        var split = strCommand.Split ( ' ' );
+        var command = split [ 0 ];
+        var args2 = split.Skip ( 1 ).ToArray ();
+
+        Rexide.Log ( $"'{strCommand}' | '{string.Join ( " ", args2 )}'" );
+
 
         foreach ( var cmd in Rexide.Instance?.AllConsoleCommands )
         {
-            if ( cmd.Command == strCommand )
+            if ( cmd.Command == command )
             {
                 try
                 {
-                    cmd.Callback?.Invoke ( options.Connection?.player as BasePlayer, strCommand, args3 );
+                    cmd.Callback?.Invoke ( options.Connection?.player as BasePlayer, command, args2 );
                 }
                 catch ( Exception ex )
                 {
                     Rexide.Error ( "ConsoleSystem_Run", ex );
                 }
+
+                break;
             }
         }
-
-        Facepunch.Pool.Free ( ref args3 );
-        Facepunch.Pool.FreeList ( ref args2 );
     }
 }
