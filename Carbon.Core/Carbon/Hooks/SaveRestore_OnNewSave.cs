8d27ab8c6730ed75efb975f153ab87f22833c645
David <davidgrin1337@gmail.com>
2022-08-30 02:41:02 +0200

    OnNewSave added

---
+++ Carbon.Core/Carbon/Hooks/SaveRestore_OnNewSave.cs
@@ -0,0 +1,18 @@
+﻿using ConVar;
+using Facepunch;
+using Harmony;
+using Carbon.Core.Harmony;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+// [HarmonyPatch ( typeof ( Type ), "Method" )]
+public class Class_Method
+{
+    public static void Postfix ()
+    {
+
+    }
+}
