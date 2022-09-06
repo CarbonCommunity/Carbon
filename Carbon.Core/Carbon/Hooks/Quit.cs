806e81fe236c1e5095fbc8c7d4b8f1efb86acbf0
raul <raulssorban@gmail.com>
2022-09-06 15:15:09 +0200

    Unload plugins when server quits Version++

---
+++ Carbon.Core/Carbon/Hooks/Quit.cs
@@ -0,0 +1,12 @@
+﻿using Carbon.Core;
+using Harmony;
+
+[HarmonyPatch ( typeof ( ConVar.Global ), "quit" )]
+public class Quit
+{
+    public static void Prefix ( ConsoleSystem.Arg args )
+    {
+        CarbonCore.Instance.HarmonyProcessor.Clear ();
+        CarbonCore.Instance.PluginProcessor.Clear ();
+    }
+}
