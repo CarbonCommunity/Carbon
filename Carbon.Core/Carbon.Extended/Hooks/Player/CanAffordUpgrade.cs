e69c2b5bdbd34e2b4408f3ff20e419b88bf07f42
raul <raulssorban@gmail.com>
2022-09-15 01:59:15 +0200

    Added CanAffordUpgrade, CanAssignBed

---
+++ Carbon.Core/Carbon.Extended/Hooks/Player/CanAffordUpgrade.cs
@@ -0,0 +1,21 @@
+﻿using Carbon.Core;
+using Harmony;
+
+namespace Carbon.Extended
+{
+    [HarmonyPatch ( typeof ( BuildingBlock ), "CanAffordUpgrade" )]
+    public class BuildingBlock_CanAffordUpgrade
+    {
+        public static bool Prefix ( BuildingGrade.Enum iGrade, BasePlayer player, ref BuildingBlock __instance, ref bool __result )
+        {
+            var result = HookExecutor.CallStaticHook ( "CanAffordUpgrade", player, __instance, iGrade );
+
+            if ( result != null )
+            {
+                __result = ( bool )result;
+            }
+
+            return result == null;
+        }
+    }
+}
---
+++ Carbon.Core/Carbon.Extended/Hooks/Player/CanAssignBed.cs
@@ -0,0 +1,33 @@
+﻿using Carbon.Core;
+using Harmony;
+
+namespace Carbon.Extended
+{
+    [HarmonyPatch ( typeof ( SleepingBag ), "AssignToFriend" )]
+    public class SleepingBag_CanAffordUpgrade
+    {
+        public static bool Prefix ( BaseEntity.RPCMessage msg, ref SleepingBag __instance )
+        {
+            if ( !msg.player.CanInteract () )
+            {
+                return true;
+            }
+
+            if ( __instance.deployerUserID != msg.player.userID )
+            {
+                return true;
+            }
+
+            var oldPosition = msg.read.Position;
+            var userId = msg.read.UInt64 ();
+            msg.read.Position = oldPosition;
+
+            if ( userId == 0UL )
+            {
+                return true;
+            }
+
+            return HookExecutor.CallStaticHook ( "CanAssignBed", msg.player, __instance, userId ) == null;
+        }
+    }
+}
