7646710621b92c4cbdf74523fa0c71ca5d807260
raul <raulssorban@gmail.com>
2022-09-15 16:08:46 +0200

    Compile fix

--- Carbon.Core/Carbon.Extended/Hooks/Player/OnLootEntityEnd.cs
+++ Carbon.Core/Carbon.Extended/Hooks/Player/OnLootEntityEnd.cs
@@ -19,7 +19,7 @@ namespace Carbon.Extended
     [Hook ( "OnLootEntityEnd" ), Hook.Category ( Hook.Category.Enum.Player )]
     [Hook.Parameter ( "player", typeof ( BasePlayer ) )]
     [Hook.Parameter ( "this", typeof ( StorageContainer ) )]
-    [Hook.Info ( "Called when the player stops looting an entity." )
+    [Hook.Info ( "Called when the player stops looting an entity." )]
     [HarmonyPatch ( typeof ( StorageContainer ), "PlayerStoppedLooting" )]
     public class StorageContainer_PlayerStoppedLooting
     {
@@ -32,7 +32,7 @@ namespace Carbon.Extended
     [Hook ( "OnLootEntityEnd" ), Hook.Category ( Hook.Category.Enum.Player )]
     [Hook.Parameter ( "player", typeof ( BasePlayer ) )]
     [Hook.Parameter ( "this", typeof ( ItemBasedFlowRestrictor ) )]
-    [Hook.Info ( "Called when the player stops looting an entity." )
+    [Hook.Info ( "Called when the player stops looting an entity." )]
     [HarmonyPatch ( typeof ( ItemBasedFlowRestrictor ), "PlayerStoppedLooting" )]
     public class ItemBasedFlowRestrictor_PlayerStoppedLooting
     {
@@ -45,7 +45,7 @@ namespace Carbon.Extended
     [Hook ( "OnLootEntityEnd" ), Hook.Category ( Hook.Category.Enum.Player )]
     [Hook.Parameter ( "player", typeof ( BasePlayer ) )]
     [Hook.Parameter ( "this", typeof ( DroppedItemContainer ) )]
-    [Hook.Info ( "Called when the player stops looting an entity." )
+    [Hook.Info ( "Called when the player stops looting an entity." )]
     [HarmonyPatch ( typeof ( DroppedItemContainer ), "PlayerStoppedLooting" )]
     public class DroppedItemContainer_PlayerStoppedLooting
     {
@@ -58,7 +58,7 @@ namespace Carbon.Extended
     [Hook ( "OnLootEntityEnd" ), Hook.Category ( Hook.Category.Enum.Player )]
     [Hook.Parameter ( "player", typeof ( BasePlayer ) )]
     [Hook.Parameter ( "this", typeof ( ContainerIOEntity ) )]
-    [Hook.Info ( "Called when the player stops looting an entity." )
+    [Hook.Info ( "Called when the player stops looting an entity." )]
     [HarmonyPatch ( typeof ( ContainerIOEntity ), "PlayerStoppedLooting" )]
     public class ContainerIOEntity_PlayerStoppedLooting
     {
