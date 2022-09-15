2ae78cdb7cfbc8380ffbab3e0d4e0c168548aae3
raul <raulssorban@gmail.com>
2022-09-15 02:12:11 +0200

    Added OnPlayerRecover, OnPlayerRecovered

---
+++ Carbon.Core/Carbon.Extended/Hooks/Player/OnPlayerRecover_OnPlayerRecovered.cs
@@ -0,0 +1,19 @@
+﻿using Carbon.Core;
+using Harmony;
+
+namespace Carbon.Extended
+{
+    [HarmonyPatch ( typeof ( BasePlayer ), "RecoverFromWounded" )]
+    public class BasePlayer_RecoverFromWounded
+    {
+        public static bool Prefix ( ref BasePlayer __instance )
+        {
+            return HookExecutor.CallStaticHook ( "OnPlayerRecover", __instance ) == null;
+        }
+
+        public static void Postfix ( ref BasePlayer __instance )
+        {
+            HookExecutor.CallStaticHook ( "OnPlayerRecovered", __instance );
+        }
+    }
+}
