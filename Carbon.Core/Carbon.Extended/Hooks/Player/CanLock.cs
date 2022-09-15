1f5a3e6ed8be601c7537ac87b4ae01a294c95824
raul <raulssorban@gmail.com>
2022-09-15 02:15:42 +0200

    Added CanLock (CodeLock, KeyLock)

---
+++ Carbon.Core/Carbon.Extended/Hooks/Player/CanLock.cs
@@ -0,0 +1,53 @@
+﻿using Carbon.Core;
+using ConVar;
+using Harmony;
+using Oxide.Core;
+
+namespace Carbon.Extended
+{
+    [HarmonyPatch ( typeof ( KeyLock ), "Lock" )]
+    public class KeyLock_Lock
+    {
+        public static bool Prefix ( BasePlayer player, ref KeyLock __instance )
+        {
+            if ( player == null )
+            {
+                return true;
+            }
+
+            if ( !player.CanInteract () )
+            {
+                return true;
+            }
+
+            if ( __instance.IsLocked () )
+            {
+                return true;
+            }
+
+            return HookExecutor.CallStaticHook ( "CanLock", player, __instance ) == null;
+        }
+    }
+
+    [HarmonyPatch ( typeof ( CodeLock ), "TryLock" )]
+    public class CodeLock_Lock
+    {
+        public static bool Prefix ( BaseEntity.RPCMessage rpc, ref CodeLock __instance )
+        {
+            if ( !rpc.player.CanInteract () )
+            {
+                return true;
+            }
+            if ( __instance.IsLocked () )
+            {
+                return true;
+            }
+            if ( __instance.code.Length != 4 )
+            {
+                return true;
+            }
+
+            return HookExecutor.CallStaticHook ( "CanLock", rpc.player, __instance ) == null;
+        }
+    }
+}
