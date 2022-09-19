3078610cbaee2257f2962f822814a390b664ee6a
Joao Brazio <jbrazio@gmail.com>
2022-09-19 19:27:15 +0100

    Moved UnityDoorstop to a specific folder

--- Tools/Scripts/dev_init.bat
+++ Tools/Scripts/dev_init.bat
@@ -6,11 +6,14 @@ git submodule update
 
 rem Build Steam Downloading Utility
 dotnet restore .\Tools\DepotDownloader
-dotnet build .\Tools\DepotDownloadern --no-incremental -c Release 
+dotnet build .\Tools\DepotDownloadern --no-incremental -c Release
 
 rem Build .NET Assembly stripper, publicizer and general utility tool
 dotnet restore .\Tools\NStrip
-dotnet build .\Tools\NStrip --no-incremental -c Release 
+dotnet build .\Tools\NStrip --no-incremental -c Release
+
+rem Keeping Unity DoorStop out of the game for now due to the more complex
+rem build process.
 
 rem Download rust binary libs
 .\Tools\DepotDownloader\DepotDownloader\bin\Release\net6.0\DepotDownloader.exe ^
