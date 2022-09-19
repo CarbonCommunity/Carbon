48ce426e5e271490f052346e3a5248358b0f84ec
Joao Brazio <jbrazio@gmail.com>
2022-09-19 23:44:16 +0100

    Batch scripts

--- Tools/Scripts/dev_init.bat
+++ Tools/Scripts/dev_init.bat
@@ -1,20 +1,21 @@
 @echo off
 
+set SCRIPTPATH=%~dp0..\..
+
 rem Inits and downloads the submodules
 git submodule init
 git submodule update
 
 rem Build Steam Downloading Utility
-dotnet restore .\Tools\DepotDownloader
-dotnet build .\Tools\DepotDownloadern --no-incremental -c Release
+dotnet restore %SCRIPTPATH%\Tools\DepotDownloader
+dotnet build %SCRIPTPATH%\Tools\DepotDownloader --no-incremental -c Release
 
 rem Build .NET Assembly stripper, publicizer and general utility tool
-dotnet restore .\Tools\NStrip
-dotnet build .\Tools\NStrip --no-incremental -c Release
+dotnet restore %SCRIPTPATH%\Tools\NStrip
+dotnet build %SCRIPTPATH%\Tools\NStrip --no-incremental -c Release
 
 rem Keeping Unity DoorStop out of the game for now due to the more complex
 rem build process.
 
 rem Download rust binary libs
-.\Tools\DepotDownloader\DepotDownloader\bin\Release\net6.0\DepotDownloader.exe ^
-	-app 258550 -branch public -depot 258551 -filelist .\Tools\Helpers\258550_258551_refs.txt -dir Rust .\Rust
+call %~dp0\dev_update.bat
