127f93c295ab06ba0147f29eda561b0fb04c265d
Joao Brazio <jbrazio@gmail.com>
2022-09-19 18:32:15 +0100

    Dev env init script and rust lib refs download

---
+++ Tools/Scripts/dev_init.bat
@@ -0,0 +1,17 @@
+@echo off
+
+rem Inits and downloads the submodules
+git submodule init
+git submodule update
+
+rem Build Steam Downloading Utility
+dotnet restore .\Tools\DepotDownloader
+dotnet build .\Tools\DepotDownloadern --no-incremental -c Release 
+
+rem Build .NET Assembly stripper, publicizer and general utility tool
+dotnet restore .\Tools\NStrip
+dotnet build .\Tools\NStrip --no-incremental -c Release 
+
+rem Download rust binary libs
+.\Tools\DepotDownloader\DepotDownloader\bin\Release\net6.0\DepotDownloader.exe ^
+	-app 258550 -branch public -depot 258551 -filelist .\Tools\Helpers\258550_258551_refs.txt -dir Rust .\Rust
