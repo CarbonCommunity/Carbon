5ea2428193b9e94d5bbb8819ffaf6c4436b656d0
raul <raulssorban@gmail.com>
2022-09-20 03:43:02 +0200

    Fixed patching

--- Carbon.Core/Carbon.Patch/Program.cs
+++ Carbon.Core/Carbon.Patch/Program.cs
@@ -11,8 +11,8 @@ namespace Carbon.Patch
             try { OsEx.Folder.DeleteContents ( "Release" ); } catch { }
             OsEx.Folder.Create ( $"Release" );
 
-            OsEx.File.Copy ( "Carbon.Core/Carbon/bin/Release/net48/Carbon.dll", "Release/Carbon.dll" );
-            OsEx.File.Copy ( "Carbon.Core/Carbon/bin/ReleaseUnix/net48/Carbon.dll", "Release/Carbon-Unix.dll" );
+            OsEx.File.Copy ( "../../Carbon.Core/Carbon/bin/Release/net48/Carbon.dll", "Release/Carbon.dll" );
+            OsEx.File.Copy ( "../../Carbon.Core/Carbon/bin/ReleaseUnix/net48/Carbon.dll", "Release/Carbon-Unix.dll" );
 
             //
             // Windows patch
@@ -21,11 +21,11 @@ namespace Carbon.Patch
             {
                 using ( var archive = new ZipArchive ( memoryStream, ZipArchiveMode.Create, true ) )
                 {
-                    archive.CreateEntryFromFile ( "Carbon.Core/Carbon/bin/Release/net48/Carbon.dll", "HarmonyMods/Carbon.dll" );
-                    archive.CreateEntryFromFile ( "Carbon.Core/Carbon.Doorstop/bin/Release/net48/Carbon.Doorstop.dll", "RustDedicated_Data/Managed/Carbon.Doorstop.dll" );
-                    archive.CreateEntryFromFile ( "Tools/Helpers/doorstop_config.ini", "doorstop_config.ini" );
-                    archive.CreateEntryFromFile ( "Tools/UnityDoorStop/winhttp.dll", "winhttp.dll" );
-                    archive.CreateEntryFromFile ( "Tools/NStrip/NStrip/bin/Release/net452/NStrip.exe", "carbon/tools/NStrip.exe" );
+                    archive.CreateEntryFromFile ( "../../Carbon.Core/Carbon/bin/Release/net48/Carbon.dll", "HarmonyMods/Carbon.dll" );
+                    archive.CreateEntryFromFile ( "../../Carbon.Core/Carbon.Doorstop/bin/Release/net48/Carbon.Doorstop.dll", "RustDedicated_Data/Managed/Carbon.Doorstop.dll" );
+                    archive.CreateEntryFromFile ( "../Helpers/doorstop_config.ini", "doorstop_config.ini" );
+                    archive.CreateEntryFromFile ( "../UnityDoorStop/winhttp.dll", "winhttp.dll" );
+                    archive.CreateEntryFromFile ( "../NStrip/NStrip/bin/Release/net452/NStrip.exe", "carbon/tools/NStrip.exe" );
                 }
 
                 var output = $"Release/Carbon.Patch.zip";
@@ -46,9 +46,9 @@ namespace Carbon.Patch
             {
                 using ( var archive = new ZipArchive ( memoryStream, ZipArchiveMode.Create, true ) )
                 {
-                    archive.CreateEntryFromFile ( "Carbon.Core/Carbon/bin/ReleaseUnix/net48/Carbon.dll", "HarmonyMods/Carbon-Unix.dll" );
-                    archive.CreateEntryFromFile ( "Tools/Scripts/linux_prepatch.sh", "carbon_prepatch.sh" );
-                    archive.CreateEntryFromFile ( "Tools/NStrip/NStrip/bin/Release/net452/NStrip.exe", "carbon/tools/NStrip" );
+                    archive.CreateEntryFromFile ( "../../Carbon.Core/Carbon/bin/ReleaseUnix/net48/Carbon.dll", "HarmonyMods/Carbon-Unix.dll" );
+                    archive.CreateEntryFromFile ( "../../Tools/Scripts/linux_prepatch.sh", "carbon_prepatch.sh" );
+                    archive.CreateEntryFromFile ( "../../Tools/NStrip/NStrip/bin/Release/net452/NStrip.exe", "carbon/tools/NStrip" );
                 }
 
                 var output = $"Release/Carbon.Patch-Unix.zip";
