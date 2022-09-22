9db89d2162c91cb73e0e7911ec3b3466064a5725
Joao Brazio <jbrazio@gmail.com>
2022-09-22 10:27:20 +0100

    Fixes a path typo for winhttp.dll

--- Carbon.Core/Carbon.Patch/src/FileList.cs
+++ Carbon.Core/Carbon.Patch/src/FileList.cs
@@ -12,24 +12,24 @@ namespace Carbon.Patch
 
 		private static Dictionary<string, string> windowsList = new Dictionary<string, string>
 		{
-			// tools
-			{ "%BASE%/Tools/UnityDoorStop/winhttp.dll", "winhttp.dll" },
-			{ "%BASE%/Tools/Helpers/doorstop_config.ini", "doorstop_config.ini" },
-			{ "%BASE%/Tools/NStrip/NStrip/bin/Release/net452/NStrip.exe", "carbon/tools/NStrip.exe" },
-			
 			// carbon
 			{ "%BASE%/Carbon.Core/Carbon/bin/%TARGET%/net48/Carbon.dll", "HarmonyMods/Carbon.dll" },
 			{ "%BASE%/Carbon.Core/Carbon.Doorstop/bin/%TARGET%/net48/Carbon.Doorstop.dll", "RustDedicated_Data/Managed/Carbon.Doorstop.dll" },
+
+			// tools
+			{ "%BASE%/Tools/UnityDoorstop/winhttp.dll", "winhttp.dll" },
+			{ "%BASE%/Tools/Helpers/doorstop_config.ini", "doorstop_config.ini" },
+			{ "%BASE%/Tools/NStrip/NStrip/bin/Release/net452/NStrip.exe", "carbon/tools/NStrip.exe" },
 		};
 
 		private static Dictionary<string, string> unixList = new Dictionary<string, string>
 		{
-			// tools
-			{ "%BASE%/Tools/NStrip/NStrip/bin/Release/net452/NStrip.exe", "carbon/tools/NStrip.exe" },
-
 			// carbon
 			{ "%BASE%/Tools/Helpers/linux_prepatch.sh", "carbon_prepatch.sh" },
 			{ "%BASE%/Carbon.Core/Carbon/bin/%TARGET%Unix/net48/Carbon.dll", "HarmonyMods/Carbon-Unix.dll" },
+
+			// tools
+			{ "%BASE%/Tools/NStrip/NStrip/bin/Release/net452/NStrip.exe", "carbon/tools/NStrip.exe" },
 		};
 	}
 }
