ee491f818a54ff07b6eaa6da128c3878cff6c944
raul <raulssorban@gmail.com>
2022-09-23 01:15:48 +0200

    Linux signtool

--- Tools/Build/linux/dev_release.sh
+++ Tools/Build/linux/dev_release.sh
@@ -23,5 +23,18 @@ dotnet   clean ${ROOT}/Carbon.Core --configuration ${TARGET} --nologo
 dotnet   build ${ROOT}/Carbon.Core --configuration ${TARGET} --no-restore --no-incremental
 dotnet   build ${ROOT}/Carbon.Core --configuration ${TARGET}Unix --no-restore --no-incremental
 
+rem Update Assembly version
+mono ${ROOT}/Carbon.Core/Carbon.Patch/bin/${TARGET}/net48/Carbon.Patch.exe --path ${ROOT} --versionupdate
+
+# Rebuild the solution
+dotnet   build ${ROOT}/Carbon.Core --configuration ${TARGET} --no-restore --no-incremental
+dotnet   build ${ROOT}/Carbon.Core --configuration ${TARGET}Unix --no-restore --no-incremental
+
+CERT=${ROOT}/Tools/Humanlights.SignTool/Certificate/carbon
+echo ${PFXCERT)> ${CERT}.pfx_base64
+base64 --decode ${CERT}.pfx_base64 ${CERT}.pfx
+
+mono ${ROOT}/Tools/Humanlights.SignTool/Humanlights.SignTool.exe sign -folder "${ROOT}/Carbon.Core/Carbon/bin" -certificate "${CERT}.pfx" -altcertificate "${CERT}.cer" +password "${CERTPASS}" /da "sha256"
+
 # Create the patch file(s)
 mono ${ROOT}/Carbon.Core/Carbon.Patch/bin/${TARGET}/net48/Carbon.Patch.exe --path ${ROOT} --configuration ${TARGET}
