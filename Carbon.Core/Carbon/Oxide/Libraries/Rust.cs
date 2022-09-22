4dfcd4a9022ab943b85fda35b525ee8a26bfb2e2
raul <raulssorban@gmail.com>
2022-09-22 21:09:53 +0200

    Added Rust, Player and Server Oxide-like libraries

---
+++ Carbon.Core/Carbon/Oxide/Libraries/Rust.cs
@@ -0,0 +1,39 @@
+﻿using Oxide.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oxide.Game.Rust.Libraries
+{
+    public class Rust
+    {
+        internal readonly Player Player = new Player ();
+        internal readonly Server Server = new Server ();
+
+        public string QuoteSafe ( string str )
+        {
+            return str.Quote ();
+        }
+
+        public void BroadcastChat ( string name, string message = null, string userId = "0" )
+        {
+            Server.Broadcast ( message, name, Convert.ToUInt64 ( userId ) );
+        }
+
+        public void SendChatMessage ( BasePlayer player, string name, string message = null, string userId = "0" )
+        {
+            Player.Message ( player, message, name, Convert.ToUInt64 ( userId ) );
+        }
+
+        public void RunClientCommand ( BasePlayer player, string command, params object [] args )
+        {
+            Player.Command ( player, command, args );
+        }
+        public void RunServerCommand ( string command, params object [] args )
+        {
+            Server.Command ( command, args );
+        }
+    }
+}
---
+++ Carbon.Core/Carbon/Oxide/Libraries/Server.cs
@@ -0,0 +1,31 @@
+﻿using Oxide.Core.Libraries.Covalence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oxide.Game.Rust.Libraries
+{
+    public class Server
+    {
+        public void Broadcast ( string message, string prefix, ulong userId = 0uL, params object [] args )
+        {
+            if ( !string.IsNullOrEmpty ( message ) )
+            {
+                message = ( ( args.Length != 0 ) ? string.Format ( Formatter.ToUnity ( message ), args ) : Formatter.ToUnity ( message ) );
+                string text = ( ( prefix != null ) ? ( prefix + ": " + message ) : message );
+                ConsoleNetwork.BroadcastToAllClients ( "chat.add", 2, userId, text );
+            }
+        }
+        public void Broadcast ( string message, ulong userId = 0uL )
+        {
+            Broadcast ( message, null, userId );
+        }
+
+        public void Command ( string command, params object [] args )
+        {
+            ConsoleSystem.Run ( ConsoleSystem.Option.Server, command, args );
+        }
+    }
+}
