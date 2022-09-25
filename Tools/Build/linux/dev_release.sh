e98463b1c2ae1624eaef353d2fd3abab76de75e9
raul <raulssorban@gmail.com>
2022-09-25 03:47:25 +0200

    Removed duplicated build call

--- Tools/Build/linux/dev_release.sh
+++ Tools/Build/linux/dev_release.sh
@@ -23,9 +23,5 @@ dotnet   clean ${ROOT}/Carbon.Core --configuration ${TARGET} --nologo
 dotnet   build ${ROOT}/Carbon.Core --configuration ${TARGET} --no-restore --no-incremental
 dotnet   build ${ROOT}/Carbon.Core --configuration ${TARGET}Unix --no-restore --no-incremental
 
-# Rebuild the solution
-dotnet   build ${ROOT}/Carbon.Core --configuration ${TARGET} --no-restore --no-incremental
-dotnet   build ${ROOT}/Carbon.Core --configuration ${TARGET}Unix --no-restore --no-incremental
-
 # Create the patch file(s)
 mono ${ROOT}/Carbon.Core/Carbon.Patch/bin/${TARGET}/net48/Carbon.Patch.exe --path ${ROOT} --configuration ${TARGET}
