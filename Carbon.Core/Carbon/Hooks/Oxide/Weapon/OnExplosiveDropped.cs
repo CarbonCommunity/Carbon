7b62505d7447849e72a31ebd2bb0217f27ce284d
raul <raulssorban@gmail.com>
2022-09-27 01:01:20 +0200

    Added OnExplosiveDropped, OnExplosiveThrown

---
+++ Carbon.Core/Carbon/Hooks/Oxide/Weapon/OnExplosiveDropped.cs
@@ -0,0 +1,76 @@
+﻿///
+/// Copyright (c) 2022 Carbon Community 
+/// All rights reserved
+/// 
+
+using Carbon.Core;
+using Harmony;
+using Oxide.Core;
+using UnityEngine;
+
+namespace Carbon.Extended
+{
+    [OxideHook ( "OnExplosiveDropped" ), OxideHook.Category ( OxideHook.Category.Enum.Weapon )]
+    [OxideHook.Parameter ( "player", typeof ( BasePlayer ) )]
+    [OxideHook.Parameter ( "entity", typeof ( BaseEntity ) )]
+    [OxideHook.Parameter ( "item", typeof ( ThrownWeapon ) )]
+    [OxideHook.Info ( "Called when the player drops an explosive item (C4, grenade, ...)." )]
+    [OxideHook.Patch ( typeof ( ThrownWeapon ), "DoDrop" )]
+    public class ThrownWeapon_DoDrop
+    {
+        public static bool Prefix ( BaseEntity.RPCMessage msg, ref ThrownWeapon __instance )
+        {
+            if ( !__instance.HasItemAmount () || __instance.HasAttackCooldown () )
+            {
+                return true;
+            }
+            Vector3 vector = msg.read.Vector3 ();
+            Vector3 normalized = msg.read.Vector3 ().normalized;
+            if ( msg.player.isMounted || msg.player.HasParent () )
+            {
+                vector = msg.player.eyes.position;
+            }
+            else if ( !__instance.ValidateEyePos ( msg.player, vector ) )
+            {
+                return true;
+            }
+            global::BaseEntity baseEntity = global::GameManager.server.CreateEntity ( __instance.prefabToThrow.resourcePath, vector, Quaternion.LookRotation ( Vector3.up ), true );
+            if ( baseEntity == null )
+            {
+                return true;
+            }
+            RaycastHit hit;
+            if ( __instance.canStick && UnityEngine.Physics.SphereCast ( new Ray ( vector, normalized ), 0.05f, out hit, 1.5f, 1236478737 ) )
+            {
+                Vector3 point = hit.point;
+                Vector3 normal = hit.normal;
+                global::BaseEntity baseEntity2 = hit.GetEntity ();
+                Collider collider = hit.collider;
+                if ( baseEntity2 && baseEntity2 is global::StabilityEntity && baseEntity is global::TimedExplosive )
+                {
+                    baseEntity2 = baseEntity2.ToServer<global::BaseEntity> ();
+                    global::TimedExplosive timedExplosive = baseEntity as global::TimedExplosive;
+                    timedExplosive.onlyDamageParent = true;
+                    timedExplosive.DoStick ( point, normal, baseEntity2, collider );
+                }
+                else
+                {
+                    baseEntity.SetVelocity ( normalized );
+                }
+            }
+            else
+            {
+                baseEntity.SetVelocity ( normalized );
+            }
+            baseEntity.creatorEntity = msg.player;
+            baseEntity.skinID = __instance.skinID;
+            baseEntity.Spawn ();
+            __instance.SetUpThrownWeapon ( baseEntity );
+            __instance.StartAttackCooldown ( __instance.repeatDelay );
+            Interface.CallHook ( "OnExplosiveDropped", msg.player, baseEntity, __instance );
+            __instance.UseItemAmount ( 1 );
+
+            return false;
+        }
+    }
+}
---
+++ Carbon.Core/Carbon/Hooks/Oxide/Weapon/OnExplosiveThrown.cs
@@ -0,0 +1,100 @@
+﻿///
+/// Copyright (c) 2022 Carbon Community 
+/// All rights reserved
+/// 
+
+using Carbon.Core;
+using Harmony;
+using Oxide.Core;
+using UnityEngine;
+
+namespace Carbon.Extended
+{
+    [OxideHook ( "OnExplosiveThrown" ), OxideHook.Category ( OxideHook.Category.Enum.Weapon )]
+    [OxideHook.Parameter ( "player", typeof ( BasePlayer ) )]
+    [OxideHook.Parameter ( "entity", typeof ( BaseEntity ) )]
+    [OxideHook.Parameter ( "item", typeof ( ThrownWeapon ) )]
+    [OxideHook.Info ( "Called when the player throws an explosive item (C4, grenade, ...)." )]
+    [OxideHook.Patch ( typeof ( ThrownWeapon ), "DoThrow" )]
+    public class ThrownWeapon_DoThrow
+    {
+        public static bool Prefix ( BaseEntity.RPCMessage msg, ref ThrownWeapon __instance )
+        {
+            if ( !__instance.HasItemAmount () || __instance.HasAttackCooldown () )
+            {
+                return true;
+            }
+
+            var vector = msg.read.Vector3 ();
+            var normalized = msg.read.Vector3 ().normalized;
+            var d = Mathf.Clamp01 ( msg.read.Float () );
+
+            if ( msg.player.isMounted || msg.player.HasParent () )
+            {
+                vector = msg.player.eyes.position;
+            }
+            else if ( !__instance.ValidateEyePos ( msg.player, vector ) )
+            {
+                return true;
+            }
+
+            var baseEntity = global::GameManager.server.CreateEntity ( __instance.prefabToThrow.resourcePath, vector, Quaternion.LookRotation ( ( this.overrideAngle == Vector3.zero ) ? ( -normalized ) : this.overrideAngle ), true );
+            if ( baseEntity == null )
+            {
+                return true;
+            }
+
+            baseEntity.creatorEntity = msg.player;
+            baseEntity.skinID = __instance.skinID;
+            baseEntity.SetVelocity ( __instance.GetInheritedVelocity ( msg.player ) + normalized * __instance.maxThrowVelocity * d + msg.player.estimatedVelocity * 0.5f );
+            if ( __instance.tumbleVelocity > 0f )
+            {
+                baseEntity.SetAngularVelocity ( new Vector3 ( UnityEngine.Random.Range ( -1f, 1f ), UnityEngine.Random.Range ( -1f, 1f ), UnityEngine.Random.Range ( -1f, 1f ) ) * this.tumbleVelocity );
+            }
+
+            baseEntity.Spawn ();
+            __instance.SetUpThrownWeapon ( baseEntity );
+            __instance.StartAttackCooldown ( __instance.repeatDelay );
+            Interface.CallHook ( "OnExplosiveThrown", msg.player, baseEntity, __instance );
+            __instance.UseItemAmount ( 1 );
+
+            var player = msg.player;
+
+            if ( player != null )
+            {
+                var timedExplosive = baseEntity as global::TimedExplosive;
+                if ( timedExplosive != null )
+                {
+                    float num = 0f;
+                    foreach ( Rust.DamageTypeEntry damageTypeEntry in timedExplosive.damageTypes )
+                    {
+                        num += damageTypeEntry.amount;
+                    }
+                    Rust.Ai.Sense.Stimulate ( new Rust.Ai.Sensation
+                    {
+                        Type = Rust.Ai.SensationType.ThrownWeapon,
+                        Position = player.transform.position,
+                        Radius = 50f,
+                        DamagePotential = num,
+                        InitiatorPlayer = player,
+                        Initiator = player,
+                        UsedEntity = timedExplosive
+                    } );
+                    return true;
+                }
+                Rust.Ai.Sense.Stimulate ( new Rust.Ai.Sensation
+                {
+                    Type = Rust.Ai.SensationType.ThrownWeapon,
+                    Position = player.transform.position,
+                    Radius = 50f,
+                    DamagePotential = 0f,
+                    InitiatorPlayer = player,
+                    Initiator = player,
+                    UsedEntity = __instance
+                } );
+            }
+
+            return false;
+        }
+    }
+}
