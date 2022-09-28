a7e1e2e953a128efcb5fb4f47f6779e2ad446408
raul <raulssorban@gmail.com>
2022-09-28 17:19:39 +0200

    Added AutoWipe initial module

---
+++ Carbon.Core/Carbon/Modules/AutoWipeModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbon.Core.Modules
+{
+    public class AutoWipeModule : BaseModule<AutoWipeConfig>
+    {
+        public override string Id => "AutoWipe";
+    }
+
+    public class AutoWipeConfig
+    {
+
+    }
+}
