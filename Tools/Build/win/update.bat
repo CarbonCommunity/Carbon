bbd59e2086509d558388f5899a3455fbf16c4dc2
Joao Brazio <jbrazio@gmail.com>
2022-09-28 19:45:23 +0100

    Native steam os libs

--- Tools/Build/win/update.bat
+++ Tools/Build/win/update.bat
@@ -23,14 +23,16 @@ if "%1" EQU "" (
 rem Cleans the exiting files
 git clean -fx "%ROOT%\Rust"
 
-rem Download rust binary libs
-"%ROOT%\Tools\DepotDownloader\DepotDownloader\bin\Release\net6.0\DepotDownloader.exe" ^
-	-validate -app 258550 -depot 258551 -branch %TARGET% -filelist ^
-	"%ROOT%\Tools\Helpers\258550_258551_refs.txt" -dir "%ROOT%\Rust"
+FOR %%O IN (windows linux) DO (
+	rem Download rust binary libs
+	"%ROOT%\Tools\DepotDownloader\DepotDownloader\bin\Release\net6.0\DepotDownloader.exe" ^
+		-os %%O -validate -app 258550 -branch %TARGET% -filelist ^
+		"%ROOT%\Tools\Helpers\258550_258551_refs.txt" -dir "%ROOT%\Rust\%%O"
 
-rem Show me all you've got baby
-"%ROOT%\Tools\NStrip\NStrip\bin\Release\net452\NStrip.exe" ^
-	--public --include-compiler-generated --keep-resources --no-strip --overwrite ^
-	--unity-non-serialized "%ROOT%\Rust\RustDedicated_Data\Managed\Assembly-CSharp.dll"
+	rem Show me all you've got baby
+	"%ROOT%\Tools\NStrip\NStrip\bin\Release\net452\NStrip.exe" ^
+		--public --include-compiler-generated --keep-resources --no-strip --overwrite ^
+		--unity-non-serialized "%ROOT%\Rust\%%O\RustDedicated_Data\Managed\Assembly-CSharp.dll"
+)
 
 dotnet restore "%ROOT%\Carbon.Core" --nologo
