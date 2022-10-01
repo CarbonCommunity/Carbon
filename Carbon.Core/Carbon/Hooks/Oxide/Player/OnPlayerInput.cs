13496f97dfe2ec51be21715674f65d96bb9b6e2c
raul <raulssorban@gmail.com>
2022-10-01 02:56:33 +0200

    Added OnPlayerTick Made OnPlayerInput returnable

--- Carbon.Core/Carbon/Hooks/Oxide/Player/OnPlayerInput.cs
+++ Carbon.Core/Carbon/Hooks/Oxide/Player/OnPlayerInput.cs
@@ -7,16 +7,30 @@ using Carbon.Core;
 
 namespace Carbon.Extended
 {
-    [OxideHook ( "OnPlayerInput" ), OxideHook.Category ( Hook.Category.Enum.Player )]
+    [OxideHook ( "OnPlayerInput", typeof ( object ) ), OxideHook.Category ( Hook.Category.Enum.Player )]
     [OxideHook.Parameter ( "player", typeof ( BasePlayer ) )]
     [OxideHook.Parameter ( "input", typeof ( InputState ) )]
     [OxideHook.Info ( "Called when input is received from a connected client." )]
     [OxideHook.Patch ( typeof ( BasePlayer ), "OnReceiveTick" )]
-    public class BasePlayer_OnReceiveTick
+    public class BasePlayer_OnReceiveTick_OnPlayerInput
     {
-        public static void Prefix ( PlayerTick msg, bool wasPlayerStalled, ref BasePlayer __instance )
+        public static bool Prefix ( PlayerTick msg, bool wasPlayerStalled, ref BasePlayer __instance )
         {
-            HookExecutor.CallStaticHook ( "OnPlayerInput", __instance, __instance.serverInput );
+            return HookExecutor.CallStaticHook ( "OnPlayerTick", __instance, msg, wasPlayerStalled ) == null ||
+                 HookExecutor.CallStaticHook ( "OnPlayerInput", __instance, __instance.serverInput ) == null;
+        }
+    }
+
+    [OxideHook ( "OnPlayerTick", typeof ( object ) ), OxideHook.Category ( Hook.Category.Enum.Player )]
+    [OxideHook.Require ( "OnPlayerInput" )]
+    [OxideHook.Parameter ( "player", typeof ( BasePlayer ) )]
+    [OxideHook.Parameter ( "tick", typeof ( PlayerTick ) )]
+    [OxideHook.Parameter ( "wasPlayerStalled", typeof ( bool ) )]
+    [OxideHook.Patch ( typeof ( BasePlayer ), "OnReceiveTick" )]
+    public class BasePlayer_OnReceiveTick_OnPlayerTick
+    {
+        public static void Prefix ( PlayerTick msg, bool wasPlayerStalled )
+        {
         }
     }
 }
