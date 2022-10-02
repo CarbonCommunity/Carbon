9c2b376e0571e10a57d8aadb49d0dbd74bd23a52
ArtIOMI <arti2895@gmail.com>
2022-10-02 21:36:51 +0200

    Add hooks for Sprinkler and WaterBall - bool CanWaterBallSplash(ItemDefinition liquidDef, Vector3 position, float radius, int amount) - void OnSprinklerSplashed(Sprinkler sprinkler)

---
+++ Carbon.Core/Carbon/Hooks/Oxide/Entity/OnSprinklerSplash.cs
@@ -0,0 +1,22 @@
+﻿///
+/// Copyright (c) 2022 Carbon Community 
+/// All rights reserved
+/// 
+
+using Carbon.Core;
+
+namespace Carbon.Extended
+{
+	[OxideHook("OnSprinklerSplashed"), OxideHook.Category(Hook.Category.Enum.Entity)]
+	[OxideHook.Parameter("this", typeof(Sprinkler))]
+	[OxideHook.Info("Called after any sprinkler has splashed water.")]
+	[OxideHook.Info("No return behavior.")]
+	[OxideHook.Patch(typeof(Sprinkler), "DoSplash")]
+	public class Sprinkler_DoSplash
+	{
+		public static void Postfix(ref Sprinkler __instance)
+		{
+			HookExecutor.CallStaticHook("OnSprinklerSplashed", __instance);
+		}
+	}
+}
