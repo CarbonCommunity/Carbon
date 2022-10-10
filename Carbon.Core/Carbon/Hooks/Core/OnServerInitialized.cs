64be151b0a4165858e2fd7605c5a73ad442ad099
raul <raulssorban@gmail.com>
2022-10-10 23:50:53 +0200

    Updated OnServerInitialized hook

--- Carbon.Core/Carbon/Hooks/Core/OnServerInitialized.cs
+++ Carbon.Core/Carbon/Hooks/Core/OnServerInitialized.cs
@@ -5,8 +5,12 @@
 
 using Carbon.Core;
 
-[Hook.AlwaysPatched, Hook.Hidden]
-[Hook("IOnServerInit"), Hook.Category(Hook.Category.Enum.Core)]
+[Hook.AlwaysPatched]
+[Hook.Parameter("booted", typeof(bool), true)]
+[Hook.Info("Called after the server startup has been completed and is awaiting connections.")]
+[Hook.Info("Also called for plugins that are hotloaded while the server is already started running.")]
+[Hook.Info("Boolean parameter, false if called on plugin hotload and true if called on server initialization.")]
+[Hook("OnServerInitialized"), Hook.Category(Hook.Category.Enum.Server)]
 [Hook.Patch(typeof(ServerMgr), "OpenConnection")]
 public class OnServerInitialized
 {
