dcd7735a6d000797289254306737deaeabe2caf3
Joao Brazio <jbrazio@gmail.com>
2022-10-14 10:40:53 +0100

    Build scripts tweak and cleanup

--- Carbon.Core/Carbon.Patch/src/FileList.cs
+++ Carbon.Core/Carbon.Patch/src/FileList.cs
@@ -6,15 +6,15 @@ namespace Carbon.Patch
 	{
 		private static Dictionary<string, string> commonList = new Dictionary<string, string>
 		{
-			{ "%BASE%/Carbon.Core/Carbon/bin/%TARGET%/net48/Carbon.dll", "%BASE%/Release/Carbon.dll" },
-			{ "%BASE%/Carbon.Core/Carbon/bin/%TARGET%Unix/net48/Carbon.dll", "%BASE%/Release/Carbon-Unix.dll" },
+			{ "%BASE%/Carbon.Core/Carbon/bin/%TARGET%/Carbon.dll", "%BASE%/Release/Carbon.dll" },
+			{ "%BASE%/Carbon.Core/Carbon/bin/%TARGET%Unix/Carbon.dll", "%BASE%/Release/Carbon-Unix.dll" },
 		};
 
 		private static Dictionary<string, string> windowsList = new Dictionary<string, string>
 		{
 			// carbon
-			{ "%BASE%/Carbon.Core/Carbon/bin/%TARGET%/net48/Carbon.dll", "HarmonyMods/Carbon.dll" },
-			{ "%BASE%/Carbon.Core/Carbon.Doorstop/bin/%TARGET%/net48/Carbon.Doorstop.dll", "RustDedicated_Data/Managed/Carbon.Doorstop.dll" },
+			{ "%BASE%/Carbon.Core/Carbon/bin/%TARGET%/Carbon.dll", "HarmonyMods/Carbon.dll" },
+			{ "%BASE%/Carbon.Core/Carbon.Doorstop/bin/%TARGET%/Carbon.Doorstop.dll", "RustDedicated_Data/Managed/Carbon.Doorstop.dll" },
 
 			// tools
 			{ "%BASE%/Tools/Helpers/doorstop_config.ini", "doorstop_config.ini" },
@@ -25,7 +25,8 @@ namespace Carbon.Patch
 		{
 			// carbon
 			{ "%BASE%/Tools/Helpers/publicizer.sh", "carbon/tools/publicizer.sh" },
-			{ "%BASE%/Carbon.Core/Carbon/bin/%TARGET%Unix/net48/Carbon.dll", "HarmonyMods/Carbon-Unix.dll" },
+			{ "%BASE%/Carbon.Core/Carbon/bin/%TARGET%Unix/Carbon.dll", "HarmonyMods/Carbon-Unix.dll" },
+			{ "%BASE%/Carbon.Core/Carbon.Doorstop/bin/%TARGET%Unix/Carbon.Doorstop.dll", "HarmonyMods/Carbon.Doorstop.dll" },
 		};
 	}
 }
