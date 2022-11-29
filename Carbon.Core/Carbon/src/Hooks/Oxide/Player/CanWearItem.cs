fe62c8af0c5142f012332fe6d7b09ac3e4c8b6f2
raul <raulssorban@gmail.com>
2022-11-30 00:51:30 +0100

    Added CanWearItem cuz david said so

---
+++ Carbon.Core/Carbon/src/Hooks/Oxide/Player/CanWearItem.cs
@@ -0,0 +1,40 @@
+﻿
+using Oxide.Core;
+using ProtoBuf;
+
+///
+/// Copyright (c) 2022 Carbon Community 
+/// All rights reserved
+/// 
+
+namespace Carbon.Hooks
+{
+	[OxideHook("CanWearItem", typeof(object)), OxideHook.Category(Hook.Category.Enum.Player)]
+	[OxideHook.Parameter("inventory", typeof(PlayerInventory))]
+	[OxideHook.Parameter("item", typeof(Item))]
+	[OxideHook.Parameter("targetSlot", typeof(int))]
+	[OxideHook.Info("Called when the player attempts to equip an item.")]
+	[OxideHook.Patch(typeof(PlayerInventory), "CanWearItem")]
+	public class PlayerInventory_CanWearItem
+	{
+		public static bool Prefix(Item item, int targetSlot, ref PlayerInventory __instance, out bool __result)
+		{
+			var component = item.info.GetComponent<ItemModWearable>();
+			if (component == null)
+			{
+				__result = false;
+				return false;
+			}
+
+			var obj = Interface.CallHook("CanWearItem", __instance, item, targetSlot);
+			if (obj is bool boolObj)
+			{
+				__result = boolObj;
+				return (bool)obj;
+			}
+
+			__result = default;
+			return true;
+		}
+	}
+}
