5024ef4ded7098a96b8cfc3584ed9a2b2bfae468
raul <raulssorban@gmail.com>
2023-01-30 18:52:23 +0100

    Initial Admin module

---
+++ Carbon.Core/Carbon/src/Modules/AdminModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Carbon.Base;
+using Newtonsoft.Json;
+
+/*
+ *
+ * Copyright (c) 2022-2023 Carbon Community 
+ * All rights reserved.
+ *
+ */
+
+namespace Carbon.Modules;
+
+public class AdminModule : CarbonModule<AdminConfig, AdminData>
+{
+	public override string Name => "Admin";
+	public override Type Type => typeof(AdminModule);
+	public override bool EnabledByDefault => true;
+
+
+}
+
+public class AdminConfig
+{
+
+}
+public class AdminData
+{
+
+}
