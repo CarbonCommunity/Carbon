ec05a66557c94de7df84667c020b3a157a426a31
raul <raulssorban@gmail.com>
2023-02-08 15:15:24 +0100

    Added AddPlugin(RustPlugin), RemovePlugin(RustPlugin), GetPlugin(string) and GetPlugins() into PluginManager

--- Carbon.Core/Carbon/src/Oxide/PluginManager.cs
+++ Carbon.Core/Carbon/src/Oxide/PluginManager.cs
@@ -1,4 +1,8 @@
-﻿using Carbon.Core;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Carbon;
+using Carbon.Core;
+using Oxide.Plugins;
 
 /*
  *
@@ -10,4 +14,36 @@
 public class PluginManager
 {
 	public string ConfigPath => Defines.GetConfigsFolder();
+
+	public bool AddPlugin(RustPlugin plugin)
+	{
+		if (!Community.Runtime.Plugins.Plugins.Any(x => x == plugin))
+		{
+			Community.Runtime.Plugins.Plugins.Add(plugin);
+			return true;
+		}
+
+		return false;
+	}
+
+	public bool RemovePlugin(RustPlugin plugin)
+	{
+		if(Community.Runtime.Plugins.Plugins.Any(x => x == plugin))
+		{
+			Community.Runtime.Plugins.Plugins.Remove(plugin);
+			return true;
+		}
+
+		return false;
+	}
+
+	public Plugin GetPlugin(string name)
+	{
+		return Community.Runtime.Plugins.Plugins.FirstOrDefault(x => x.Name == name);
+	}
+
+	public IEnumerable<Plugin> GetPlugins()
+	{
+		return Community.Runtime.Plugins.Plugins.AsEnumerable();
+	}
 }
