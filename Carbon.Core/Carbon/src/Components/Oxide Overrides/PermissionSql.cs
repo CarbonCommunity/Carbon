95e958bf14294d6fb5359c0e3ecbb5ea729d90de
raul <raulssorban@gmail.com>
2023-02-13 11:45:40 +0100

    Perm sql licensing

--- Carbon.Core/Carbon/src/Components/Oxide Overrides/PermissionSql.cs
+++ Carbon.Core/Carbon/src/Components/Oxide Overrides/PermissionSql.cs
@@ -1,5 +1,12 @@
 ﻿using Oxide.Core.Libraries;
 
+/*
+ *
+ * Copyright (c) 2022-2023 Carbon Community 
+ * All rights reserved.
+ *
+ */
+
 namespace Carbon.Oxide;
 
 public class PermissionSql : Permission
