642d3ad9339e10e1b972acc2477cef7d1412d1ff
raul <raulssorban@gmail.com>
2023-02-20 22:00:19 +0100

    Full map protection implementation

--- Carbon.Core/Carbon/src/Modules/MapProtectionModule.cs
+++ Carbon.Core/Carbon/src/Modules/MapProtectionModule.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Carbon.Base;
+using Carbon.Core;
 using Carbon.Extensions;
-using Newtonsoft.Json;
 using ProtoBuf;
 using UnityEngine;
 
@@ -23,38 +23,59 @@ public class MapProtectionModule : CarbonModule<MapProtectionConfig, MapProtecti
 	public override Type Type => typeof(MapProtectionModule);
 	public override bool EnabledByDefault => true;
 
-	[ConsoleCommand("docleanup")]
-	private void DoCleanup(ConsoleSystem.Arg arg)
+	private void OnServerInitialized()
 	{
-		Cleanup();
+		Process();
 	}
 
-	private void Cleanup()
+	private void Process()
 	{
 		if (!OsEx.File.Exists(Config.Key)) return;
 
-		var key = Key.Deserialize(Config.Key);
-		Puts($"Deserialized key: {Config.Key} with {key.points.Count:n0} points");
-
-		var entKilled = 0;
-		foreach (var entity in BaseNetworkable.serverEntities)
+		try
 		{
-			if (key.points.Any(x => x == entity.transform.position))
+			var key = Key.Deserialize(Config.Key);
+			var temporaryEntities = BaseNetworkable.serverEntities.ToArray();
+			var entitiesObliterated = 0;
+
+			Puts($"Unlocking map with key '{Path.GetFileName(Config.Key)}'. Processing {key.points.Count:n0} points...");
+
+			using (TimeMeasure.New("MapProtectionModule.Process"))
 			{
-				try { entity.Kill(); entKilled++; }
-				catch
+				foreach (var entity in temporaryEntities)
 				{
-					try { entity.AdminKill(); entKilled++; } catch { }
-				}		
+					for (int i = 0; i < key.points.Count; i++)
+					{
+						if (key.points[i] == entity.transform.position)
+						{
+							if (!entity.IsDestroyed)
+							{
+								entity.Kill();
+								entitiesObliterated++;
+							}
+
+							break;
+						}
+					}
+				}
 			}
+
+			Array.Clear(temporaryEntities, 0, temporaryEntities.Length);
+			temporaryEntities = null;
+
+			Puts($"Successfully unlocked map.");
 		}
+		catch (Exception exception) { PutsWarn($"Failed to successfully unlock map. Please report the following error:\n{exception}"); }
 	}
 
-	[ProtoContract]
+	[ProtoBuf.ProtoContract]
 	public class Key : IDisposable
 	{
 		[ProtoMember(1)]
-		public List<VectorData> points;
+		public List<Vector> points;
+
+		[ProtoMember(2)]
+		public uint size;
 
 		public void Serialize(string file)
 		{
@@ -77,6 +98,44 @@ public class MapProtectionModule : CarbonModule<MapProtectionConfig, MapProtecti
 		{
 			points?.Clear();
 			points = null;
+		}
+
+		[ProtoBuf.ProtoContract]
+		public class Vector
+		{
+			[ProtoMember(1)]
+			public float x;
+
+			[ProtoMember(2)]
+			public float y;
+
+			[ProtoMember(3)]
+			public float z;
+
+			public Vector() { }
+			public Vector(float x, float y, float z)
+			{
+				this.x = x;
+				this.y = y;
+				this.z = z;
+			}
+
+			public static implicit operator Vector(Vector3 v)
+			{
+				return new Vector(v.x, v.y, v.z);
+			}
+			public static implicit operator Vector(Quaternion q)
+			{
+				return q.eulerAngles;
+			}
+			public static implicit operator Vector3(Vector v)
+			{
+				return new Vector3(v.x, v.y, v.z);
+			}
+			public static implicit operator Quaternion(Vector v)
+			{
+				return Quaternion.Euler(v);
+			}
 		}
 	}
 }
