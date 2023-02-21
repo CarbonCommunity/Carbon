9b2e1d19f22875d10dc9991d19a1ffb1d0e2a41a
raul <raulssorban@gmail.com>
2023-02-22 00:34:30 +0100

    Fixed identifier

--- Carbon.Core/Carbon.Hooks.Base/src/Core/World/IOnWorldSerializationSave.cs
+++ Carbon.Core/Carbon.Hooks.Base/src/Core/World/IOnWorldSerializationSave.cs
@@ -15,11 +15,11 @@ public partial class Category_World
 	public partial class World_WorldSerialization
 	{
 		[HookAttribute.Patch("IOnWorldSerializationSave", typeof(WorldSerialization), "Save", new System.Type[] { typeof(string) })]
-		[HookAttribute.Identifier("46e7586c847e42b392ec0c14b75b9451")]
+		[HookAttribute.Identifier("0c164134a9b646bfa8c1085705952205")]
 
 		// Called before and after WorldSerialization is saved.
 
-		public class World_WorldSerialization_Save_46e7586c847e42b392ec0c14b75b9451
+		public class World_WorldSerialization_Save_0c164134a9b646bfa8c1085705952205
 		{
 			public static void Prefix(string fileName, ref WorldSerialization __instance)
 			{
