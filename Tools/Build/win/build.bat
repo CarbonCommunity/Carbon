b53f547c56c0497aed4fd302a2253987d66b303a
Joao Brazio <jbrazio@gmail.com>
2023-02-23 12:04:06 +0000

    Move interop handling to msbuild targets

--- Tools/Build/win/build.bat
+++ Tools/Build/win/build.bat
@@ -15,6 +15,7 @@ if "%1" EQU "" (
 ) else (
 	set TARGET=%1
 )
+
 echo ** Set the build target config to %TARGET%
 
 echo ** Cleanup the release folder
@@ -28,29 +29,20 @@ dotnet   build "%ROOT%\Carbon.Core" -v:m --configuration %TARGET% --no-restore -
 
 echo ** Copy operating system specific files
 echo "%TARGET%" | findstr /C:"Unix" >NUL && (
-	copy /y "%ROOT%\Tools\Helpers\environment.sh"                 						"%ROOT%\Release\.tmp\%TARGET%\carbon\tools\"
-	copy /y "%ROOT%\Tools\UnityDoorstop\linux\x64\libdoorstop.so" 						"%ROOT%\Release\.tmp\%TARGET%"
-	robocopy "%ROOT%\Carbon.Core\Carbon\bin\%TARGET%\x64" 						  		"%ROOT%\Release\.tmp\%TARGET%\carbon\managed\lib\x64" /E
-	robocopy "%ROOT%\Carbon.Core\Carbon\bin\%TARGET%\x86" 						  		"%ROOT%\Release\.tmp\%TARGET%\carbon\managed\lib\x86" /E
+	copy /y "%ROOT%\Tools\Helpers\environment.sh"                 "%ROOT%\Release\.tmp\%TARGET%\carbon\tools\"
+	copy /y "%ROOT%\Tools\UnityDoorstop\linux\x64\libdoorstop.so" "%ROOT%\Release\.tmp\%TARGET%"
+
 	"%ROOT%\Tools\BuildInfo\Carbon.BuildInfo.exe" -carbon "%ROOT%\Release\.tmp\%TARGET%\carbon\managed\Carbon.dll" -o "%ROOT%\Release\.tmp\%TARGET%\build-unix.txt"
-	(CALL )                                                                                                                     
+	(CALL )
 ) || (                                                                                                                          
-	copy /y "%ROOT%\Tools\Helpers\doorstop_config.ini"            						"%ROOT%\Release\.tmp\%TARGET%"                                
-	copy /y "%ROOT%\Tools\UnityDoorstop\windows\x64\doorstop.dll" 						"%ROOT%\Release\.tmp\%TARGET%\winhttp.dll"                    
-	robocopy "%ROOT%\Carbon.Core\Carbon\bin\%TARGET%\x64" 						  		"%ROOT%\Release\.tmp\%TARGET%\carbon\managed\lib\x64" /E
-	robocopy "%ROOT%\Carbon.Core\Carbon\bin\%TARGET%\x86" 						  		"%ROOT%\Release\.tmp\%TARGET%\carbon\managed\lib\x86" /E
+	copy /y "%ROOT%\Tools\Helpers\doorstop_config.ini"            "%ROOT%\Release\.tmp\%TARGET%"                                
+	copy /y "%ROOT%\Tools\UnityDoorstop\windows\x64\doorstop.dll" "%ROOT%\Release\.tmp\%TARGET%\winhttp.dll"    
+
 	"%ROOT%\Tools\BuildInfo\Carbon.BuildInfo.exe" -carbon "%ROOT%\Release\.tmp\%TARGET%\carbon\managed\Carbon.dll" -o "%ROOT%\Release\.tmp\%TARGET%\build.txt"
 	(CALL )
 )
 
-:: echo ** Create the standalone files
-:: copy /y "%ROOT%\Release\.tmp\%TARGET%\HarmonyMods\Carbon.Stub.dll"        "%ROOT%\Release"
-:: copy /y "%ROOT%\Release\.tmp\%TARGET%\carbon\managed\Carbon.dll"          "%ROOT%\Release"
-:: copy /y "%ROOT%\Release\.tmp\%TARGET%\carbon\managed\Carbon.Doorstop.dll" "%ROOT%\Release"
-:: copy /y "%ROOT%\Release\.tmp\%TARGET%\carbon\managed\Carbon.Loader.dll"   "%ROOT%\Release"
-:: 
-:: copy /y "%ROOT%\Release\.tmp\%TARGET%\carbon\managed\hooks\Carbon.Hooks.Base.dll"     "%ROOT%\Release"
-:: copy /y "%ROOT%\Release\.tmp\%TARGET%\carbon\managed\hooks\Carbon.Hooks.Extended.dll" "%ROOT%\Release"
-
-echo ** Create the compressed archive
-powershell -Command "Compress-Archive -Update -Path '%ROOT%\Release\.tmp\%TARGET%\*' -DestinationPath '%ROOT%\Release\Carbon.%TARGET%.zip'"
+if "%2" NEQ "--no-archive" (
+	echo ** Create the compressed archive
+	powershell -Command "Compress-Archive -Update -Path '%ROOT%\Release\.tmp\%TARGET%\*' -DestinationPath '%ROOT%\Release\Carbon.%TARGET%.zip'"
+)
