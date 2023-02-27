17442efaffcc1a2d6dbdaeb48bafe44e701643ad
Joao Brazio <jbrazio@gmail.com>
2023-02-27 01:28:14 +0000

    Add awaitable Download task to DownloadManager

--- Carbon.Core/Carbon.Common/src/Contracts/IDownloadManager.cs
+++ Carbon.Core/Carbon.Common/src/Contracts/IDownloadManager.cs
@@ -12,5 +12,6 @@ namespace API.Contracts;
 
 public interface IDownloadManager
 {
+	public Task<byte[]> Download(string url);
 	public void DownloadAsync(string url, Action<string, byte[]> callback);
 }
