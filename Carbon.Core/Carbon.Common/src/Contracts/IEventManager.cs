cc2409128ce3a114d7fa3fff226b52f3ce6998cc
Joao Brazio <jbrazio@gmail.com>
2023-02-27 01:52:00 +0000

    Adds summary to some APIs

--- Carbon.Core/Carbon.Common/src/Contracts/IEventManager.cs
+++ Carbon.Core/Carbon.Common/src/Contracts/IEventManager.cs
@@ -12,7 +12,18 @@ namespace API.Contracts;
 
 public interface IEventManager
 {
+	/// <summary>
+	/// Subscribe to an event.
+	/// </summary>
 	public void Subscribe(CarbonEvent eventId, Action<EventArgs> callback);
+
+	/// <summary>
+	/// Trigger an event.
+	/// </summary>
 	public void Trigger(CarbonEvent eventId, EventArgs args);
+
+	/// <summary>
+	/// Unsubscribe from an event.
+	/// </summary>
 	public void Unsubscribe(CarbonEvent eventId, Action<EventArgs> callback);
 }
--- Carbon.Core/Carbon.Common/src/Events/CarbonEventArgs.cs
+++ Carbon.Core/Carbon.Common/src/Events/CarbonEventArgs.cs
@@ -11,5 +11,10 @@ namespace API.Events;
 
 public class CarbonEventArgs : EventArgs
 {
+	public object Payload { get; }
 
+	public CarbonEventArgs(object payload)
+	{
+		Payload = payload;
+	}
 }
