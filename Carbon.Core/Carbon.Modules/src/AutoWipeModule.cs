3cb8e4e35d64ec9e4e7c474cd72d8c1afc61f895
João Brázio <jbrazio@gmail.com>
2023-03-10 00:42:28 +0000

    Disabled AutoWipe until proper implementation

--- Carbon.Core/Carbon.Modules/src/AutoWipeModule.cs
+++ Carbon.Core/Carbon.Modules/src/AutoWipeModule.cs
@@ -1,26 +1,26 @@
-﻿using System;
-using Carbon.Base;
+﻿// using System;
+// using Carbon.Base;
 
-/*
- *
- * Copyright (c) 2022-2023 Carbon Community 
- * All rights reserved.
- *
- */
+// /*
+//  *
+//  * Copyright (c) 2022-2023 Carbon Community 
+//  * All rights reserved.
+//  *
+//  */
 
-namespace Carbon.Modules;
+// namespace Carbon.Modules;
 
-public class AutoWipeModule : CarbonModule<AutoWipeConfig, AutoWipeData>
-{
-	public override string Name => "AutoWipe";
-	public override Type Type => typeof(AutoWipeModule);
-}
+// public class AutoWipeModule : CarbonModule<AutoWipeConfig, AutoWipeData>
+// {
+// 	public override string Name => "AutoWipe";
+// 	public override Type Type => typeof(AutoWipeModule);
+// }
 
-public class AutoWipeConfig
-{
-	public int NextWipeSeed = -1;
-}
-public class AutoWipeData
-{
+// public class AutoWipeConfig
+// {
+// 	public int NextWipeSeed = -1;
+// }
+// public class AutoWipeData
+// {
 
-}
+// }
