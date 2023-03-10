4161b17cdb8b732c0e42af31c6b8d3c5bbb9db5b
João Brázio <jbrazio@gmail.com>
2023-03-10 01:08:41 +0000

    Add CircularNetworkDistance module

---
+++ Carbon.Core/Carbon.Modules/src/CircularNetworkDistance/Patches/NetworkVisibilityGrid.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using API.Hooks;
+using Network.Visibility;
+
+// Copyright (c) 2022-2023 Carbon Community
+// All rights reserved
+
+namespace Carbon.Modules;
+
+public partial class CircularNetworkDistance
+{
+	[HookAttribute.Patch("ae0577348a5140ea9aa861cd71c31e7c", "CircularNetworkDistance [Patch]", typeof(NetworkVisibilityGrid), "GetVisibleFrom")]
+	[HookAttribute.Identifier("ae0577348a5140ea9aa861cd71c31e7c")]
+	[HookAttribute.Options(HookFlags.Hidden)]
+	public class NetworkVisibilityGrid_GetVisibleFrom_ae0577348a5140ea9aa861cd71c31e7c : API.Hooks.Patch
+	{
+		public static bool Prefix(NetworkVisibilityGrid __instance, Group group, List<Group> groups, int radius)
+			=> GetVisibleFromCircle(__instance, group, groups, radius);
+	}
+}
