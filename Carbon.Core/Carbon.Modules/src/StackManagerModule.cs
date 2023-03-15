546230012bd439059436d0db751b891413383d25
raul <raulssorban@gmail.com>
2023-03-15 07:40:20 +0100

    Fixed StackManager module not applying on server boot

--- Carbon.Core/Carbon.Modules/src/StackManagerModule.cs
+++ Carbon.Core/Carbon.Modules/src/StackManagerModule.cs
@@ -33,6 +33,8 @@ public class StackManagerModule : CarbonModule<StackManagerConfig, StackManagerD
 		}
 
 		if (hasChanged) Save();
+
+		OnEnableStatus();
 	}
 
 	public override void OnEnabled(bool initialized)
