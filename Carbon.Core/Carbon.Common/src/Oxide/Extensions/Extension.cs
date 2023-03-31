cab8255edfc2591624b3ed3d82e90c024b1182db
raul <raulssorban@gmail.com>
2023-03-31 15:06:49 +0200

    Added ExtensionManager placebo

--- Carbon.Core/Carbon.Common/src/Oxide/Extensions/Extension.cs
+++ Carbon.Core/Carbon.Common/src/Oxide/Extensions/Extension.cs
@@ -1,21 +1,30 @@
-﻿namespace Oxide.Core.Extensions
+﻿/*
+ *
+ * Copyright (c) 2022-2023 Carbon Community 
+ * All rights reserved.
+ *
+ */
+
+namespace Oxide.Core.Extensions;
+
+public class Extension
 {
-	public class Extension
-	{
-		public  string Name { get; set; }
-		public  string Author { get; set; }
-		public  VersionNumber Version { get; set; }
-		public string Filename { get; set; }
-		public string Branch { get; set; } = "public";
+	public  string Name { get; set; }
+	public  string Author { get; set; }
+	public  VersionNumber Version { get; set; }
+	public string Filename { get; set; }
+	public string Branch { get; set; } = "public";
 
-		public bool IsCoreExtension { get; }
-		public bool IsGameExtension { get; }
-		public bool SupportsReloading { get; }
+	public bool IsCoreExtension { get; }
+	public bool IsGameExtension { get; }
+	public bool SupportsReloading { get; }
 
-		public virtual void Load() { }
-		public virtual void Unload() { }
-		public virtual void LoadPluginWatchers(string pluginDirectory) { }
-		public virtual void OnModLoad() { }
-		public virtual void OnShutdown() { }
-	}
+	public virtual void Load() { }
+	public virtual void Unload() { }
+	public virtual void LoadPluginWatchers(string pluginDirectory) { }
+	public virtual void OnModLoad() { }
+	public virtual void OnShutdown() { }
+
+	public Extension() { }
+	public Extension(ExtensionManager manager) { }
 }
