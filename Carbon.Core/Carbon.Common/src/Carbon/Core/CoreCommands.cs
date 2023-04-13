b019875e94bda827fe2d7897985c306a1b59a751
raul <raulssorban@gmail.com>
2023-04-13 05:48:03 +0200

    Added c.wipemarkers [player] which clears all markers of a player or caller

--- Carbon.Core/Carbon.Common/src/Carbon/Core/CoreCommands.cs
+++ Carbon.Core/Carbon.Common/src/Carbon/Core/CoreCommands.cs
@@ -1292,4 +1292,32 @@ public partial class CorePlugin : CarbonPlugin
 	}
 
 	#endregion
+
+	#region Markers
+
+	[ConsoleCommand("wipemarkers", "Removes all markers of the calling player or argument filter.")]
+	[AuthLevel(2)]
+	private void ClearMarkers(ConsoleSystem.Arg arg)
+	{
+		var player = arg.Player();
+
+		if (arg.HasArgs(1))
+		{
+			player = BasePlayer.FindAwakeOrSleeping(arg.GetString(0));
+		}
+
+		if (player == null)
+		{
+			arg.ReplyWith($"Couldn't find that player.");
+			return;
+		}
+
+		arg.ReplyWith(arg.IsServerside ? $"Removed {player.displayName}'s map notes." : $"Removed all map notes.");
+
+		player.Server_ClearMapMarkers(default);
+		player.SendMarkersToClient();
+		player.SendNetworkUpdate();
+	}
+
+	#endregion
 }
