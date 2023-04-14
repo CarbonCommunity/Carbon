b39c0624caff75c5fb199e0526381e02ffefe410
raul <raulssorban@gmail.com>
2023-04-14 09:42:05 +0200

    Auth

--- Carbon.Core/Carbon.Common/src/Oxide/OxideCommand.cs
+++ Carbon.Core/Carbon.Common/src/Oxide/OxideCommand.cs
@@ -14,10 +14,10 @@ public class OxideCommand : AuthenticatedCommand
 	public string Command { get { return Name; } set { Name = value; } }
 	public BaseHookable Plugin { get; set; }
 	public new Action<BasePlayer, string, string[]> Callback { get; set; }
-	public string[] Permissions { get { return Auth.Permissions; } set { Auth.Permissions = value; } }
-	public string[] Groups { get { return Auth.Groups; } set { Auth.Groups = value; } }
-	public int AuthLevel { get { return Auth.AuthLevel; } set { Auth.AuthLevel = value; } }
-	public int Cooldown { get { return Auth.Cooldown; } set { Auth.Cooldown = value; } }
+	public string[] Permissions { get { return Auth == null ? default : Auth.Permissions; } set { if (Auth != null) Auth.Permissions = value; } }
+	public string[] Groups { get { return Auth == null ? default : Auth.Groups; } set { if (Auth != null) Auth.Groups = value; } }
+	public int AuthLevel { get { return Auth == null ? default : Auth.AuthLevel; } set { if (Auth != null) Auth.AuthLevel = value; } }
+	public int Cooldown { get { return Auth == null ? default : Auth.Cooldown; } set { if (Auth != null) Auth.Cooldown = value; } }
 	public bool IsHidden { get { return HasFlag(CommandFlags.Hidden); } set { SetFlag(CommandFlags.Hidden, value); } }
 	public bool Protected { get { return HasFlag(CommandFlags.Protected); } set { SetFlag(CommandFlags.Protected, value); } }
 }
