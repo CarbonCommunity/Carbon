9c426a1c037ec3f04df7ef698ff1208508c44962
raul <raulssorban@gmail.com>
2023-05-09 18:37:49 +0200

    Added OvenSpeedOverride to GatherManager - changes the smelting speed in all ovens Added smelt speed override blacklisting (based on prefab shortname and entity type)

---
+++ Carbon.Core/Carbon.Modules/src/GatherManagerModule/Patches/IOvenSmeltSpeedOverride.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using API.Hooks;
+using UnityEngine;
+
+/*
+ *
+ * Copyright (c) 2022-2023 Carbon Community 
+ * All rights reserved.
+ *
+ */
+
+namespace Carbon.Modules;
+
+public partial class GatherManagerModule
+{
+	[HookAttribute.Patch("IOvenSmeltSpeedOverride", "IOvenSmeltSpeedOverride", typeof(BaseOven), "StartCooking", new System.Type[] { })]
+	[HookAttribute.Identifier("91cf6d3873704b12b39fd555c376a652")]
+	[HookAttribute.Options(HookFlags.Hidden)]
+
+	public class BaseOven_StartCooking_91cf6d3873704b12b39fd555c376a652 : API.Hooks.Patch
+	{
+		public static bool Prefix(BaseOven __instance)
+		{
+			if (HookCaller.CallStaticHook("IOvenSmeltSpeedOverride", __instance) is float hookResult)
+			{
+				if (__instance.FindBurnable() == null && !__instance.CanRunWithNoFuel)
+				{
+					return false;
+				}
+				__instance.inventory.temperature = __instance.cookingTemperature;
+				__instance.UpdateAttachmentTemperature();
+				__instance.InvokeRepeating(new Action(__instance.Cook), hookResult, hookResult);
+				__instance.SetFlag(global::BaseEntity.Flags.On, true, false, true);
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
