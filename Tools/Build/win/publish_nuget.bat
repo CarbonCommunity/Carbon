193286e57af223af6d1476361ebdf81bf0657058
raul <raulssorban@gmail.com>
2023-05-11 19:00:46 +0200

    Metadata update NuGet build & publishing

---
+++ Tools/Build/win/publish_nuget.bat
@@ -0,0 +1,15 @@
+::
+:: Copyright (c) 2022-2023 Carbon Community 
+:: All rights reserved
+::
+@echo off
+
+set HOME=%cd%
+set API_KEY=%1
+set VERSION=%~n2
+
+cd "%~dp0\..\..\..\Carbon.Core"
+dotnet pack -o .nugets -c Debug /p:PackageVersion=%VERSION% --no-build
+dotnet nuget push .nugets\Carbon.Community.%VERSION%.nupkg --api-key "%API_KEY%" --source "https://api.nuget.org/v3/index.json"
+
+cd "%HOME%"
