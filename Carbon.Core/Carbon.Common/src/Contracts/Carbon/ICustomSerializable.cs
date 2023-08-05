5e04d1926dcb504e7c31eeb401081818b0c2096e
raul <raulssorban@gmail.com>
2023-08-05 02:39:17 +0200

    Temporary revert

--- Carbon.Core/Carbon.Common/src/Contracts/Carbon/ICustomSerializable.cs
+++
@@ -1,9 +0,0 @@
-namespace Carbon.Contracts;
-
-public interface ICustomSerializable : IDisposable
-{
-	public void Setup(BaseEntity entity, bool justCreated);
-	public bool CanSave { get; }
-	public byte[] Serialize();
-	public void Deserialize(byte[] data);
-}
