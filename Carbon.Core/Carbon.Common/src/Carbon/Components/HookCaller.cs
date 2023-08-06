ecafe39d8bdac475fd04848d4e89f2fcd71da850
raul <raulssorban@gmail.com>
2023-08-06 23:00:23 +0200

    Don't consider static methods hookable by the code gen

--- Carbon.Core/Carbon.Common/src/Carbon/Components/HookCaller.cs
+++ Carbon.Core/Carbon.Common/src/Carbon/Components/HookCaller.cs
@@ -1343,7 +1343,7 @@ public static class HookCaller
 		}
 
 		var hookableMethods = new Dictionary<uint, List<MethodDeclarationSyntax>>();
-		var privateMethods0 = methodDeclarations.Where(md => (md.Modifiers.Count == 0 || md.Modifiers.All(modifier => !modifier.IsKind(SyntaxKind.PublicKeyword)) || md.AttributeLists.Any(x => x.Attributes.Any(y => y.Name.ToString() == "HookMethod"))) && md.TypeParameterList == null);
+		var privateMethods0 = methodDeclarations.Where(md => (md.Modifiers.Count == 0 || md.Modifiers.All(modifier => !modifier.IsKind(SyntaxKind.PublicKeyword) && !modifier.IsKind(SyntaxKind.StaticKeyword)) || md.AttributeLists.Any(x => x.Attributes.Any(y => y.Name.ToString() == "HookMethod"))) && md.TypeParameterList == null);
 		var privateMethods = privateMethods0.OrderBy(x => x.Identifier.ValueText);
 		privateMethods0 = null;
 
