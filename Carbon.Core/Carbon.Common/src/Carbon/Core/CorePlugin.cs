af67c6aded19278ea936b0d3549c21aa54ae85c6
raul <raulssorban@gmail.com>
2023-08-16 01:12:41 +0200

    No disconnect double log

--- Carbon.Core/Carbon.Common/src/Carbon/Core/CorePlugin.cs
+++ Carbon.Core/Carbon.Common/src/Carbon/Core/CorePlugin.cs
@@ -748,7 +748,6 @@ public partial class CorePlugin : CarbonPlugin
 	private void OnPlayerDisconnected(BasePlayer player, string reason)
 	{
 		HookCaller.CallStaticHook(4253366379, player?.AsIPlayer(), reason);
-		Logger.Log($"{player.net.connection} left: {reason}");
 	
 		if (player.IsAdmin && !player.IsOnGround())
 		{
