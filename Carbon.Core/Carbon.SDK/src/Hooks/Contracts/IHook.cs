17e740971d02314a2f40e271cce117292d6a4bf7
raul <raulssorban@gmail.com>
2023-08-15 22:31:05 +0200

    Exposed `IHook.HookFlags`, `IHook.TargetType`, `IHook.TargetMethod`, `IHook.TargetMethods`

--- Carbon.Core/Carbon.SDK/src/Hooks/Contracts/IHook.cs
+++ Carbon.Core/Carbon.SDK/src/Hooks/Contracts/IHook.cs
@@ -5,6 +5,10 @@
  *
  */
 
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
 namespace API.Hooks;
 
 public interface IHook
@@ -14,9 +18,14 @@ public interface IHook
 	bool IsPatch { get; }
 	bool IsStaticHook { get; }
 
+	HookFlags Options { get; set; }
 	HookState Status { get; }
 
 	string HookFullName { get; }
 	string HookName { get; }
 	string Identifier { get; }
+
+	Type TargetType { get; }
+	string TargetMethod { get; }
+	List<MethodBase> TargetMethods { get; }
 }
