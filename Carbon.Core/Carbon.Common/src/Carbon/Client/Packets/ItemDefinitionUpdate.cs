7347c7d2a1e3412e88ee58f4680f4717a01d2d88
raul <raulssorban@gmail.com>
2023-08-16 02:27:43 +0200

    Added ItemDefinition update packet [Client]

---
+++ Carbon.Core/Carbon.Common/src/Carbon/Client/Packets/ItemDefinitionUpdate.cs
@@ -0,0 +1,37 @@
+﻿using ProtoBuf;
+using ProtoBuf.Meta;
+
+namespace Carbon.Client.Packets;
+
+[ProtoContract]
+public class ItemDefinitionUpdate : BasePacket
+{
+	// static ItemDefinitionUpdate() => RuntimeTypeModel.Default[typeof(BasePacket)].AddSubType(102, typeof(ItemDefinitionUpdate));
+
+	[ProtoMember(1)]
+	public string Shortname { get; set; }
+
+	[ProtoMember(2)]
+	public string DisplayName { get; set; }
+
+	[ProtoMember(3)]
+	public string DisplayDescription{ get; set; }
+
+	public void Apply()
+	{
+		var item = ItemManager.FindItemDefinition(Shortname);
+
+		if (item == null)
+		{
+			return;
+		}
+
+		item.displayName.english = DisplayName;
+		item.displayDescription.english = DisplayDescription;
+	}
+
+	public override void Dispose()
+	{
+
+	}
+}
