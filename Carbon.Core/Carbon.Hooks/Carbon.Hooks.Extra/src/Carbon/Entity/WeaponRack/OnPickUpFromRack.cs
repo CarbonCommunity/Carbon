e86f15167ff0819c50d1895df424ef3e3037ed4f
raul <raulssorban@gmail.com>
2023-08-23 09:21:03 +0200

    Fixed `OnPickUpFromRack` hook

--- Carbon.Core/Carbon.Hooks/Carbon.Hooks.Extra/src/Carbon/Entity/WeaponRack/OnPickUpFromRack.cs
+++ Carbon.Core/Carbon.Hooks/Carbon.Hooks.Extra/src/Carbon/Entity/WeaponRack/OnPickUpFromRack.cs
@@ -19,14 +19,14 @@ public partial class Category_Entity
 
 		[MetadataAttribute.Info("Whenever a weapon was picked up from the rack.")]
 		[MetadataAttribute.Parameter("rack", typeof(WeaponRack))]
-		[MetadataAttribute.Parameter("slot", typeof(WeaponRackSlot))]
 		[MetadataAttribute.Parameter("item", typeof(Item))]
+		[MetadataAttribute.Parameter("targetSlot", typeof(int))]
 
 		public class Entity_WeaponRack_218d76c79e534841b0eacf66932f1769 : Patch
 		{
-			public static void Postfix(BasePlayer player, int mountSlotIndex, int playerBeltIndex, bool tryHold, bool sendUpdate, WeaponRack __instance)
+			public static void Postfix(Item item, int targetSlot, WeaponRack __instance)
 			{
-				HookCaller.CallStaticHook(1687007383, __instance, __instance.GetWeaponAtIndex(mountSlotIndex), tryHold ? player.GetActiveItem() : null);
+				HookCaller.CallStaticHook(1687007383, __instance, item, targetSlot);
 			}
 		}
 	}
