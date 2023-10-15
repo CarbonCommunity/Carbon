2acaf9b1bd8d98866710b9e613b79ab9f35ebb24
raul <raulssorban@gmail.com>
2023-10-15 18:47:52 +0200

    Runtime folder Runtime download

--- Tools/Build/win/update.bat
+++ Tools/Build/win/update.bat
@@ -6,6 +6,7 @@
 
 pushd %~dp0..\..\..
 set UPDATE_ROOT=%CD%
+set RUNTIME_URL=https://carbonmod.gg/assets/content/runtime.zip
 popd
 
 rem Get the target depot argument
@@ -18,6 +19,12 @@ if "%1" EQU "" (
 "%UPDATE_ROOT%\Tools\Helpers\CodeGen.exe" ^
 	--coreplugininput "%UPDATE_ROOT%\Carbon.Core\Carbon.Components\Carbon.Common\src\Carbon\Core" ^
 	--corepluginoutput "%UPDATE_ROOT%\Carbon.Core\Carbon.Components\Carbon.Common\src\Generated\CorePlugin-Generated.cs"
+
+echo Downloading Runtime from '%RUNTIME_URL%'..
+
+call powershell -Command "(New-Object Net.WebClient).DownloadFile('%RUNTIME_URL%', '%UPDATE_ROOT%\Tools\Runtime\runtime.zip')" || exit /b
+call powershell -Command "Expand-Archive '%UPDATE_ROOT%\Tools\Runtime\runtime.zip' -DestinationPath '%UPDATE_ROOT%\Tools\Runtime'" -Force
+del "%UPDATE_ROOT%\Tools\Runtime\runtime.zip"
 
 echo Downloading depots using '%UPDATE_TARGET%' Steam branch..
 
