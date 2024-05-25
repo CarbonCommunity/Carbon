39c72b38b26f04188d60edfd19b1e9fbec4e0666
Roger Karlsen <mr.haggbart@gmail.com>
2024-05-25 21:21:23 +0200

    Bump submodule SteamRE/DepotDownloader

--- Tools/Build/linux/update.sh
+++ Tools/Build/linux/update.sh
@@ -42,7 +42,7 @@ done
 
 for OS in windows linux; do
 	# Download rust binary libs
-	"${ROOT}/Tools/DepotDownloader/DepotDownloader/bin/Release/net6.0/DepotDownloader" \
+	"${ROOT}/Tools/DepotDownloader/DepotDownloader/bin/Release/net8.0/DepotDownloader" \
 		-os ${OS} -validate -app 258550 -branch ${TARGET} -filelist \
 		"${ROOT}/Tools/Helpers/258550_refs.txt" -dir "${ROOT}/Rust/${OS}"
 
