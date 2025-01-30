5fde1d9afd06ac1c93908911058da9951eb08870
raul <raulssorban@gmail.com>
2025-01-30 21:39:55 +0100

    Module tab updates

--- src/Carbon/Modules/AdminModule/AdminModule.Tabs.Modules.cs
+++ src/Carbon/Modules/AdminModule/AdminModule.Tabs.Modules.cs
@@ -15,143 +15,130 @@ public partial class AdminModule
 			Enabled
 		}
 
-		public static string[] SortTypeNames = Enum.GetNames(typeof(SortTypes));
+		private static string[] _sortTypeNames = Enum.GetNames(typeof(SortTypes));
+
+		private static string[] _configBlacklist = new[]
+		{
+			"Version"
+		};
 
 		public static Tab Get()
 		{
 			var tab = (Tab)null;
-			void Draw(PlayerSession ap)
-			{
-				tab.AddColumn(0, true);
-				tab.AddColumn(1, true);
-
-				var searchInput = ap.GetStorage<string>(tab, "search")?.ToLower();
-
-				tab.AddInput(0, "Search", ap => searchInput, (ap, args) =>
-				{
-					ap.SetStorage(tab, "search", args.ToString(" "));
-					Draw(ap);
-				});
-
-				var sort = (SortTypes)ap.GetStorage(tab, "sorttype", 0);
-				var sortFlip = ap.GetStorage(tab, "sortflip", false);
-
-				tab.AddDropdown(0, "Sorting", ap => (int)sort, (ap, index) =>
-				{
-					if ((int)sort != index)
-					{
-						ap.SetStorage(tab, "sortflip", false);
-						ap.SetStorage(tab, "sorttype", index);
-					}
-					else
-					{
-						ap.SetStorage(tab, "sortflip", !sortFlip);
-					}
-
-					Draw(ap);
-				}, SortTypeNames);
-
-				tab.AddName(0, "Core Modules");
-				Generate(x => x.ForceEnabled && (ap.HasStorage(tab, "search") && !string.IsNullOrEmpty(searchInput) ? x.Name.ToLower().Contains(searchInput) : true));
-
-				tab.AddName(0, "Other Modules");
-				Generate(x => !x.ForceEnabled && (ap.HasStorage(tab, "search") && !string.IsNullOrEmpty(searchInput) ? x.Name.ToLower().Contains(searchInput) : true));
-
-				void Generate(Func<BaseModule, bool> condition)
-				{
-					IEnumerable<BaseHookable> modules = sort switch
-					{
-						SortTypes.Name =>  Community.Runtime.ModuleProcessor.Modules.OrderBy(x => x.Name),
-						SortTypes.Enabled =>  Community.Runtime.ModuleProcessor.Modules.OrderByDescending(x => x is BaseModule module && module.IsEnabled()),
-						_ => Community.Runtime.ModuleProcessor.Modules
-					};
-
-					if (sortFlip)
-					{
-						modules = modules.Reverse();
-					}
-
-					foreach (var hookable in modules)
-					{
-						if (hookable is BaseModule module)
-						{
-							if (!condition(module)) continue;
-
-							tab.AddButtonArray(0,
-								new Tab.OptionButton(hookable.Name, ap =>
-								{
-									ap.SetStorage(tab, "selectedmodule", module);
-									Draw(ap);
-									DrawModuleSettings(tab, module, ap);
-								}, type: ap => ap.GetStorage<BaseModule>(tab, "selectedmodule") == module ? Tab.OptionButton.Types.Selected : Tab.OptionButton.Types.None),
-								new Tab.OptionButton($"{(module.ForceEnabled ? "Always Enabled" : module.IsEnabled() ? "Enabled" : "Disabled")}", ap =>
-								{
-									if (module.ForceEnabled) return;
-
-									module.SetEnabled(!module.IsEnabled());
-									module.Save();
-									ap.SetStorage(tab, "selectedmodule", module);
-									Draw(ap);
-									DrawModuleSettings(tab, module, ap);
-								}, type: ap => module.ForceEnabled ? Tab.OptionButton.Types.Warned : module.IsEnabled() ? Tab.OptionButton.Types.Selected : Tab.OptionButton.Types.None));
-						}
-					}
-				}
-			}
 
 			tab = new Tab("modules", "Modules", Community.Runtime.Core, access: "modules.use", onChange: (ap, tab) =>
 			{
-				ap.ClearStorage(tab, "selectedmodule");
-				Draw(ap);
+				Draw(tab, ap);
 			});
 
 			return tab;
 		}
 
-		internal static string[] _configBlacklist = new[]
+		static void Draw(Tab tab, PlayerSession ap)
 		{
-			"Version"
-		};
+			tab.AddColumn(0, true);
+			tab.AddColumn(1, true);
 
-		internal static void DrawModuleSettings(Tab tab, BaseModule module, PlayerSession ap)
-		{
-			tab.ClearColumn(1);
+			var searchInput = ap.GetStorage<string>(tab, "search")?.ToLower();
 
-			tab.AddInput(1, "Name", ap => module.Name, null);
-
-			if (!module.ForceEnabled)
+			tab.AddInput(0, "Search", ap => searchInput, (ap, args) =>
 			{
-				tab.AddToggle(1, "Enabled", ap2 => { module.SetEnabled(!module.IsEnabled()); module.Save(); DrawModuleSettings(tab, module, ap); }, ap2 => module.IsEnabled());
-			}
+				ap.SetStorage(tab, "search", args.ToString(" "));
+				Draw(tab, ap);
+			});
 
-			tab.AddButtonArray(1,
-				new Tab.OptionButton("Save", ap => { module.Save(); }),
-				new Tab.OptionButton("Load", ap => { module.Load(); }));
+			var sort = (SortTypes)ap.GetStorage(tab, "sorttype", 0);
+			var sortFlip = ap.GetStorage(tab, "sortflip", false);
 
-			if (Singleton.HasAccess(ap.Player, "modules.config_edit"))
+			tab.AddDropdown(0, "Sorting", ap => (int)sort, (ap, index) =>
 			{
-				tab.AddButton(1, "Edit Config", ap =>
+				if ((int)sort != index)
 				{
-					var moduleConfigFile = Path.Combine(Defines.GetModulesFolder(), module.Name, "config.json");
-					ap.SelectedTab = ConfigEditor.Make(OsEx.File.ReadText(moduleConfigFile),
-						(ap, _) =>
-						{
-							Singleton.SetTab(ap.Player, "modules");
-							Singleton.Draw(ap.Player);
-						},
-						(ap, jobject) =>
-						{
-							var wasEnabled = module.IsEnabled();
-							OsEx.File.Create(moduleConfigFile, jobject.ToString(Formatting.Indented));
-							module.SetEnabled(false);
-							module.OnUnload();
-							module.Load();
-							if(wasEnabled) module.SetEnabled(wasEnabled);
+					ap.SetStorage(tab, "sortflip", false);
+					ap.SetStorage(tab, "sorttype", index);
+				}
+				else
+				{
+					ap.SetStorage(tab, "sortflip", !sortFlip);
+				}
 
-							Singleton.SetTab(ap.Player, "modules");
-							Singleton.Draw(ap.Player);
-						}, null, blacklist: _configBlacklist);
-				});
+				Draw(tab, ap);
+			}, _sortTypeNames);
+
+			tab.AddName(0, "Core Modules");
+			Generate(sort, sortFlip, tab,
+				x => x.ForceEnabled && (ap.HasStorage(tab, "search") && !string.IsNullOrEmpty(searchInput)
+					? x.Name.ToLower().Contains(searchInput)
+					: true));
+
+			tab.AddName(0, "Other Modules");
+			Generate(sort, sortFlip, tab,
+				x => !x.ForceEnabled && (ap.HasStorage(tab, "search") && !string.IsNullOrEmpty(searchInput)
+					? x.Name.ToLower().Contains(searchInput)
+					: true));
+
+			static void Generate(SortTypes sort, bool sortFlip, Tab tab, Func<BaseModule, bool> condition)
+			{
+				IEnumerable<BaseHookable> modules = sort switch
+				{
+					SortTypes.Name => Community.Runtime.ModuleProcessor.Modules.OrderBy(x => x.Name),
+					SortTypes.Enabled => Community.Runtime.ModuleProcessor.Modules.OrderByDescending(x =>
+						x is BaseModule module && module.IsEnabled()),
+					_ => Community.Runtime.ModuleProcessor.Modules
+				};
+
+				if (sortFlip)
+				{
+					modules = modules.Reverse();
+				}
+
+				foreach (var hookable in modules)
+				{
+					if (hookable is not BaseModule module) continue;
+					if (!condition(module)) continue;
+
+					tab.AddButtonArray(0,
+						new Tab.OptionButton(hookable.Name, ap =>
+							{
+								Draw(tab, ap);
+							},
+							type: _ => Tab.OptionButton.Types.None),
+						new Tab.OptionButton(
+							$"{(module.ForceEnabled ? "Always Enabled" : module.IsEnabled() ? "Enabled" : "Disabled")}",
+							ap =>
+							{
+								if (module.ForceEnabled) return;
+
+								module.SetEnabled(!module.IsEnabled());
+								module.Save();
+								Draw(tab, ap);
+							},
+							type: ap => module.ForceEnabled ? Tab.OptionButton.Types.Warned :
+								module.IsEnabled() ? Tab.OptionButton.Types.Selected : Tab.OptionButton.Types.None),
+						new Tab.OptionButton("Edit Config", ap =>
+							{
+								var moduleConfigFile = Path.Combine(Defines.GetModulesFolder(), module.Name, "config.json");
+								ap.SelectedTab = ConfigEditor.Make(OsEx.File.ReadText(moduleConfigFile),
+									(ap, _) =>
+									{
+										Singleton.SetTab(ap.Player, "modules");
+										Singleton.Draw(ap.Player);
+									},
+									(ap, jobject) =>
+									{
+										var wasEnabled = module.IsEnabled();
+										OsEx.File.Create(moduleConfigFile, jobject.ToString(Formatting.Indented));
+										module.SetEnabled(false);
+										module.OnUnload();
+										module.Load();
+										if(wasEnabled) module.SetEnabled(wasEnabled);
+
+										Singleton.SetTab(ap.Player, "modules");
+										Singleton.Draw(ap.Player);
+									}, null, blacklist: _configBlacklist);
+							},
+							type: ap => Tab.OptionButton.Types.None));
+				}
 			}
 		}
 	}
