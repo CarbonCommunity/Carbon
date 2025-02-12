2b89bee6b44b993215c8e06e664bf7c0ba422ea7
bombardirs <bombardirs@hotmail.com>
2025-02-12 21:54:47 +0100

    Added CallHook implementations up to 13 args to avoid params memory allocation;

--- src/Oxide/OxideMod.cs
+++ src/Oxide/OxideMod.cs
@@ -157,27 +157,94 @@ public class OxideMod
 		return _extensions;
 	}
 
+	public object CallHook(string hookName)
+	{
+		var hookId = HookStringPool.GetOrAdd(hookName);
+		return HookCaller.CallStaticHook(hookId);
+	}
+
+	public object CallHook(string hookName, object arg1)
+	{
+	    var hookId = HookStringPool.GetOrAdd(hookName);
+	    return HookCaller.CallStaticHook(hookId, arg1);
+	}
+
+	public object CallHook(string hookName, object arg1, object arg2)
+	{
+	    var hookId = HookStringPool.GetOrAdd(hookName);
+	    return HookCaller.CallStaticHook(hookId, arg1, arg2);
+	}
+
+	public object CallHook(string hookName, object arg1, object arg2, object arg3)
+	{
+	    var hookId = HookStringPool.GetOrAdd(hookName);
+	    return HookCaller.CallStaticHook(hookId, arg1, arg2, arg3);
+	}
+
+	public object CallHook(string hookName, object arg1, object arg2, object arg3, object arg4)
+	{
+	    var hookId = HookStringPool.GetOrAdd(hookName);
+	    return HookCaller.CallStaticHook(hookId, arg1, arg2, arg3, arg4);
+	}
+
+	public object CallHook(string hookName, object arg1, object arg2, object arg3, object arg4, object arg5)
+	{
+	    var hookId = HookStringPool.GetOrAdd(hookName);
+	    return HookCaller.CallStaticHook(hookId, arg1, arg2, arg3, arg4, arg5);
+	}
+
+	public object CallHook(string hookName, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6)
+	{
+	    var hookId = HookStringPool.GetOrAdd(hookName);
+	    return HookCaller.CallStaticHook(hookId, arg1, arg2, arg3, arg4, arg5, arg6);
+	}
+
+	public object CallHook(string hookName, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6, object arg7)
+	{
+	    var hookId = HookStringPool.GetOrAdd(hookName);
+	    return HookCaller.CallStaticHook(hookId, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+	}
+
+	public object CallHook(string hookName, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6, object arg7, object arg8)
+	{
+	    var hookId = HookStringPool.GetOrAdd(hookName);
+	    return HookCaller.CallStaticHook(hookId, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
+	}
+
+	public object CallHook(string hookName, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6, object arg7, object arg8, object arg9)
+	{
+	    var hookId = HookStringPool.GetOrAdd(hookName);
+	    return HookCaller.CallStaticHook(hookId, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
+	}
+
+	public object CallHook(string hookName, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6, object arg7, object arg8, object arg9, object arg10)
+	{
+	    var hookId = HookStringPool.GetOrAdd(hookName);
+	    return HookCaller.CallStaticHook(hookId, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10);
+	}
+
+	public object CallHook(string hookName, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6, object arg7, object arg8, object arg9, object arg10, object arg11)
+	{
+	    var hookId = HookStringPool.GetOrAdd(hookName);
+	    return HookCaller.CallStaticHook(hookId, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11);
+	}
+
+	public object CallHook(string hookName, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6, object arg7, object arg8, object arg9, object arg10, object arg11, object arg12)
+	{
+	    var hookId = HookStringPool.GetOrAdd(hookName);
+	    return HookCaller.CallStaticHook(hookId, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12);
+	}
+
+	public object CallHook(string hookName, object arg1, object arg2, object arg3, object arg4, object arg5, object arg6, object arg7, object arg8, object arg9, object arg10, object arg11, object arg12, object arg13)
+	{
+	    var hookId = HookStringPool.GetOrAdd(hookName);
+	    return HookCaller.CallStaticHook(hookId, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13);
+	}
+
 	public object CallHook(string hookName, params object[] args)
 	{
 		var hookId = HookStringPool.GetOrAdd(hookName);
-
-		return args?.Length switch
-		{
-			1 => HookCaller.CallStaticHook(hookId, args[0]),
-			2 => HookCaller.CallStaticHook(hookId, args[0], args[1]),
-			3 => HookCaller.CallStaticHook(hookId, args[0], args[1], args[2]),
-			4 => HookCaller.CallStaticHook(hookId, args[0], args[1], args[2], args[3]),
-			5 => HookCaller.CallStaticHook(hookId, args[0], args[1], args[2], args[3], args[4]),
-			6 => HookCaller.CallStaticHook(hookId, args[0], args[1], args[2], args[3], args[4], args[5]),
-			7 => HookCaller.CallStaticHook(hookId, args[0], args[1], args[2], args[3], args[4], args[5], args[6]),
-			8 => HookCaller.CallStaticHook(hookId, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]),
-			9 => HookCaller.CallStaticHook(hookId, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]),
-			10 => HookCaller.CallStaticHook(hookId, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]),
-			11 => HookCaller.CallStaticHook(hookId, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10]),
-			12 => HookCaller.CallStaticHook(hookId, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11]),
-			13 => HookCaller.CallStaticHook(hookId, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11], args[12]),
-			_ => HookCaller.CallStaticHook(hookId, args, true)
-		};
+		return HookCaller.CallStaticHook(hookId, args);
 	}
 
 	public object CallDeprecatedHook(string oldHook, string newHook, DateTime expireDate, params object[] args)
