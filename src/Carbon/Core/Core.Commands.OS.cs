db8f4612ad67fbf638fd040fc0e611a1b8e6e718
raul <raulssorban@gmail.com>
2025-02-20 13:13:24 +0100

    Added `c.delete` and `c.deleteext` commands

--- src/Carbon/Core/Core.Commands.OS.cs
+++ src/Carbon/Core/Core.Commands.OS.cs
@@ -91,4 +91,29 @@ public partial class CorePlugin
 		Application.OpenURL(folder);
 		arg.ReplyWith($"Opened '{folder}'");
 	}
+
+	[ConsoleCommand("delete", "Locally deletes a file or directory relative to the server root. . Syntax: c.deleteext \"path/to\" \"cs\"")]
+	[AuthLevel(2)]
+	private void Delete(ConsoleSystem.Arg arg)
+	{
+		var path = Path.Combine(Defines.GetRootFolder(), arg.GetString(0));
+		OsEx.File.Delete(path);
+		OsEx.Folder.Delete(path);
+		arg.ReplyWith($"Deleted '{path}'");
+	}
+
+	[ConsoleCommand("deleteext", "Locally deletes all files with a specified extension relative to the server root. Syntax: c.deleteext \"path/to\" \"cs\"")]
+	[AuthLevel(2)]
+	private void DeleteExt(ConsoleSystem.Arg arg)
+	{
+		var folder = Path.Combine(Defines.GetRootFolder(), arg.GetString(0));
+		var extension = arg.GetString(1);
+
+		var files = OsEx.Folder.GetFilesWithExtension(folder, extension);
+		OsEx.Folder.DeleteFilesWithExtension(folder, extension);
+		for (int i = 0; i < files.Length; i++)
+		{
+			arg.ReplyWith($"Deleted '{files[i]}'");
+		}
+	}
 }
