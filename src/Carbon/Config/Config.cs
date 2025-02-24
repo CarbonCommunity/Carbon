5c52cf1ef51e5208c0dc2655b00ca690037beb49
raul <raulssorban@gmail.com>
2025-02-25 00:37:38 +0100

    Added initial implementation

--- src/Carbon/Config/Config.cs
+++ src/Carbon/Config/Config.cs
@@ -81,6 +81,9 @@ public class Config
 		public string PlayerDefaultGroup { get; set; } = "default";
 		public string AdminDefaultGroup { get; set; } = "admin";
 		public string ModeratorDefaultGroup { get; set; } = "moderator";
+		public bool AutoGrantPlayerGroup { get; set; } = true;
+		public bool AutoGrantAdminGroup { get; set; } = true;
+		public bool AutoGrantModeratorGroup { get; set; } = true;
 		public bool BypassAdminCooldowns { get; set; } = false;
 		public Permission.SerializationMode PermissionSerialization { get; set; } = Permission.SerializationMode.Protobuf;
 	}
