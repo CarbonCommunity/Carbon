f66575f628230152f00b60ab781e043fa336c6c6
raul <raulssorban@gmail.com>
2025-04-01 16:48:50 +0200

    Added 0All for items

--- Carbon.Core/Carbon.Tools/Carbon.Documentation/Generators/ServerMetadata.Items.cs
+++ Carbon.Core/Carbon.Tools/Carbon.Documentation/Generators/ServerMetadata.Items.cs
@@ -9,6 +9,21 @@ public partial class ServerMetadata
 		Console.WriteLine($"Found {Items.Length:n0} Rust items");
 
 		var groupedItems = Items.GroupBy(x => x.Category);
+
+		Builder.Clear();
+		Builder.AppendLine($"# All Items");
+		Builder.AppendLine($"Full list of all <Badge type=\"danger\" text=\"{Items.Count():n0}\"/> items.");
+		Builder.AppendLine();
+		foreach (var items in groupedItems)
+		{
+			Builder.AppendLine($"## {items.Key}");
+			Builder.AppendLine($"<table>");
+			StoreItems(items);
+			Builder.AppendLine($"</table>");
+			Builder.AppendLine();
+		}
+		File.WriteAllText(Path.Combine(Generator.Arguments.Docs, "references", "items", $"0All.md"), Builder.ToString());
+
 		foreach (var items in groupedItems)
 		{
 			Builder.Clear();
@@ -17,29 +32,34 @@ public partial class ServerMetadata
 			Builder.AppendLine();
 			Builder.AppendLine($"---");
 			Builder.AppendLine($"<table>");
-			foreach (var item in items)
-			{
-				Builder.AppendLine($"\t<tr {(item.Hidden ? "style=\"background-color:#300000;\"" : string.Empty)}>");
-				{
-					Builder.AppendLine($"\t\t<td style=\"width:15%;\">");
-					{
-						Builder.AppendLine($"\t\t\t<img src=\"https://carbonmod.gg/assets/media/items/{item.ShortName}.png\">");
-					}
-					Builder.AppendLine($"\t\t\t</td>");
-
-					Builder.AppendLine($"\t\t<td>");
-					{
-						var flagsText = item.Flags.ToString();
-						Builder.AppendLine($"\t<h5 id=\"{item.ShortName}\"><a href=\"{item.Category}#{item.ShortName}\"><Badge type=\"tip\" text=\"#\"/></a> {item.DisplayName} {(flagsText != "0" ? string.Join(' ', flagsText.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x => $"<Badge type=\"tip\" text=\"{x}\"/>")) : string.Empty)}</h5> ");
-						Builder.AppendLine($"\t<Badge type=\"info\" text=\"{item.Id}\"/> <Badge type=\"info\" text=\"{item.ShortName}\"/> <Badge type=\"warning\" text=\"x{item.Stack}\"/> {(item.Rarity != Structure.Rarity.None ? $"<Badge type=\"warning\" text=\"{item.Rarity}\"/>" : string.Empty)}{(item.Hidden ? $" <Badge type=\"danger\" text=\"Hidden\"/>" : string.Empty)}<br>");
-						Builder.AppendLine($"\t{item.Description}");
-					}
-					Builder.AppendLine($"</td>");
-				}
-				Builder.AppendLine($"</tr>");
-			}
+			StoreItems(items);
 			Builder.AppendLine($"</table>");
 			File.WriteAllText(Path.Combine(Generator.Arguments.Docs, "references", "items", $"{items.Key}.md"), Builder.ToString());
+		}
+	}
+
+	private static void StoreItems(IEnumerable<Structure.Item> items)
+	{
+		foreach (var item in items)
+		{
+			Builder.AppendLine($"\t<tr {(item.Hidden ? "style=\"background-color:#300000;\"" : string.Empty)}>");
+			{
+				Builder.AppendLine($"\t\t<td style=\"width:15%;\">");
+				{
+					Builder.AppendLine($"\t\t\t<img src=\"https://carbonmod.gg/assets/media/items/{item.ShortName}.png\">");
+				}
+				Builder.AppendLine($"\t\t\t</td>");
+
+				Builder.AppendLine($"\t\t<td>");
+				{
+					var flagsText = item.Flags.ToString();
+					Builder.AppendLine($"\t<h5 id=\"{item.ShortName}\"><a href=\"{item.Category}#{item.ShortName}\"><Badge type=\"tip\" text=\"#\"/></a> {item.DisplayName} {(flagsText != "0" ? string.Join(' ', flagsText.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x => $"<Badge type=\"tip\" text=\"{x}\"/>")) : string.Empty)}</h5> ");
+					Builder.AppendLine($"\t<Badge type=\"info\" text=\"{item.Id}\"/> <Badge type=\"info\" text=\"{item.ShortName}\"/> <Badge type=\"warning\" text=\"x{item.Stack}\"/> {(item.Rarity != Structure.Rarity.None ? $"<Badge type=\"warning\" text=\"{item.Rarity}\"/>" : string.Empty)}{(item.Hidden ? $" <Badge type=\"danger\" text=\"Hidden\"/>" : string.Empty)}<br>");
+					Builder.AppendLine($"\t{item.Description}");
+				}
+				Builder.AppendLine($"</td>");
+			}
+			Builder.AppendLine($"</tr>");
 		}
 	}
 }
