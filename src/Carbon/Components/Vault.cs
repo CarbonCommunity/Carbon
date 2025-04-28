ae6187b197e7b6bfa8bec22401e80821fdd038ac
raul <raulssorban@gmail.com>
2025-04-28 23:04:12 +0200

    Carbon.Vault factory rewrite Added commands for vault - c.vault - c.vault_add <key> <value> [encrypted|true] [factory|global] - c.vault_remove <key> [factory|global] A whole lot of polishing

--- src/Carbon/Components/Vault.cs
+++ src/Carbon/Components/Vault.cs
@@ -11,24 +11,26 @@ namespace Carbon.Components;
 
 public class Vault
 {
+	public static readonly string Global = "global";
+
 	private const int DEFAULT_KEY_BIT_SIZE = 256;
 	private const int DEFAULT_MAC_BIT_SIZE = 128;
 	private const int DEFAULT_NONCE_BIT_SIZE = 128;
+	private const int SALT_SIZE = 16;
 	private static readonly SecureRandom RANDOM = new();
-	private static readonly Dictionary<uint, Item> ITEMS = new();
+	private static readonly List<Factory> FACTORIES = new();
 	private static string CARBON_ID_CACHE;
 	private static string CARBON_ID => CARBON_ID_CACHE ??= JObject.Parse(OsEx.File.ReadText(Path.Combine(Defines.GetRustIdentityFolder(), "carbon.id")))["UID"].ToObject<string>();
-	private static readonly byte[] SALT = [0x26, 0xdc, 0xff, 0xfe, 0x00, 0xad, 0x7a, 0xee, 0x3c, 0xaf, 0xc5, 0x07, 0xed, 0x4d, 0x08, 0x22];
 
-	private static byte[] DecryptData(byte[] buffer, string password)
+	private static byte[] EncryptData(byte[] buffer, string password, out byte[] salt)
 	{
-		using Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(password, SALT);
-		return DecryptImpl(buffer, pdb.GetBytes(32));
-	}
-	private static byte[] EncryptData(byte[] buffer, string password)
-	{
-		using Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(password, SALT);
+		using Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(password, salt = RANDOM.GenerateSeed(SALT_SIZE));
 		return EncryptImpl(buffer, pdb.GetBytes(32));
+	}
+	private static byte[] DecryptData(byte[] buffer, string password, byte[] salt)
+	{
+		using Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(password, salt);
+		return DecryptImpl(buffer, pdb.GetBytes(32));
 	}
 
     private static byte[] DecryptImpl(byte[] encryptedMessage, byte[] key, int nonSecretPayloadLength = 0)
@@ -87,32 +89,78 @@ public class Vault
     }
     private static void ClearToPool()
     {
-	    foreach (var item in ITEMS)
+	    for (int i = 0; i < FACTORIES.Count; i++)
 	    {
-		    var element = item.Value;
-		    Facepunch. Pool.Free(ref element);
+		    var item = FACTORIES[i];
+		    Facepunch.Pool.Free(ref item);
 	    }
-	    ITEMS.Clear();
+	    FACTORIES.Clear();
     }
 
-    public static void Add(string factory, string name, string value, bool encrypted = true)
+    private static Factory CreateFactory(uint id)
+    {
+	    if (id == 0)
+	    {
+		    return null;
+	    }
+	    var factory = GetFactory(id) ?? Facepunch.Pool.Get<Factory>();
+	    factory.id = id;
+	    if (!FACTORIES.Contains(factory))
+	    {
+		    FACTORIES.Add(factory);
+	    }
+	    return factory;
+    }
+
+    public static Factory GetFactory(uint id)
+    {
+	    for (int i = 0; i < FACTORIES.Count; i++)
+	    {
+		    var factory = FACTORIES[i];
+		    if (factory.id.Equals(id))
+		    {
+			    return factory;
+		    }
+	    }
+	    return null;
+    }
+    public static List<Factory> GetFactories() => FACTORIES;
+
+    public static bool Add(string factory, string name, string value, bool encrypted = true)
     {
 	    if (string.IsNullOrEmpty(factory) || string.IsNullOrEmpty(name) || string.IsNullOrEmpty(value))
 	    {
 		    Logger.Warn("Attempted to add a null factory, name or value into the Carbon.Vault");
-		    return;
+		    return false;
 	    }
 
-	    var identifier = Pool.GetOrAdd(string.Concat(factory, name));
-	    if (!ITEMS.TryGetValue(identifier, out var item))
+	    var factoryInstance = CreateFactory(Pool.Get(factory));
+	    var identifier = Pool.Get(name);
+
+	    var salt = (byte[])default;
+	    var buffer = Encoding.UTF8.GetBytes(value);
+	    var hasItem = factoryInstance.HasItem(identifier, out var item);
+	    item ??= Facepunch.Pool.Get<Item>();
+	    item.id = identifier;
+	    item.encrypted = encrypted;
+	    item.hash = encrypted ? EncryptData(buffer, CARBON_ID, out salt) : buffer;
+	    item.salt = salt;
+	    item.cache = value;
+	    if (!hasItem)
 	    {
-		    var buffer = Encoding.UTF8.GetBytes(value);
-		    ITEMS[identifier] = item = Facepunch.Pool.Get<Item>();
-		    item.identifier = identifier;
-		    item.encrypted = encrypted;
-		    item.hash = encrypted ? EncryptData(buffer, CARBON_ID) : buffer;
-		    item.cache = value;
+		    factoryInstance.AddItem(item);
 	    }
+	    return true;
+    }
+    public static bool Remove(string factory, string name)
+    {
+	    if (string.IsNullOrEmpty(factory) || string.IsNullOrEmpty(name))
+	    {
+		    Logger.Warn("Attempted to remove a non-existent factory or factory item from the Carbon.Vault");
+		    return false;
+	    }
+
+	    return CreateFactory(Pool.Get(factory)).RemoveItem(Pool.Get(name));
     }
     public static string Get(string factory, string name)
     {
@@ -122,38 +170,58 @@ public class Vault
 		    return null;
 	    }
 
-	    var identifier = Pool.GetOrAdd(string.Concat(factory, name));
-	    if (!ITEMS.TryGetValue(identifier, out var item))
+	    var factoryInstance = CreateFactory(Pool.Get(factory));
+	    var identifier = Pool.Get(name);
+	    if (!factoryInstance.HasItem(identifier, out var item))
 	    {
 		    Logger.Warn($"Identifier with '{name}' for factory '{factory}' does not exist in the Carbon.Vault");
 		    return null;
 	    }
 	    if (string.IsNullOrEmpty(item.cache))
 	    {
-		    item.cache = Encoding.UTF8.GetString(item.encrypted ? DecryptData(item.hash, CARBON_ID) : item.hash);
+		    item.cache = Encoding.UTF8.GetString(item.encrypted ? DecryptData(item.hash, CARBON_ID, item.salt) : item.hash);
 	    }
 	    return item.cache;
     }
 
-    public static void Save()
+    public static void Save(bool silent = false)
     {
+	    var factories = 0;
+	    var items = 0;
+
 	    using var memoryStream = new MemoryStream();
 	    using (var gzipStream = new GZipStream(memoryStream, CompressionMode.Compress))
 	    {
 		    using var writer = new BinaryWriter(gzipStream);
 
-		    writer.Write(ITEMS.Count);
-		    foreach (var item in ITEMS)
+		    Pool.Save(writer);
+
+		    writer.Write(FACTORIES.Count);
+		    foreach (var factory in FACTORIES)
 		    {
-			    writer.Write(item.Value.identifier);
-			    writer.Write(item.Value.hash.Length);
-			    writer.Write(item.Value.hash);
-			    writer.Write(item.Value.encrypted);
+			    factories++;
+			    writer.Write(factory.id);
+			    writer.Write(factory.Count);
+			    foreach (var item in factory)
+			    {
+				    items++;
+				    writer.Write(item.id);
+				    writer.Write(item.hash.Length);
+				    writer.Write(item.hash);
+				    writer.Write(item.salt != null);
+				    if (item.salt != null)
+				    {
+					    writer.Write(item.salt.Length);
+					    writer.Write(item.salt);
+				    }
+				    writer.Write(item.encrypted);
+			    }
 		    }
 	    }
 	    OsEx.File.Create(Defines.GetVaultFile(), memoryStream.ToArray());
+	    if (!silent) Logger.Log($"Saved Carbon.Vault with {factories:n0} {factories.Plural("factory", "factories")} and {items:n0} {items.Plural("item", "items")}");
     }
-    public static void Load()
+    public static void Load(bool silent = false)
     {
 	    if (!OsEx.File.Exists(Defines.GetVaultFile()))
 	    {
@@ -168,15 +236,32 @@ public class Vault
 
 	    try
 	    {
-		    var count = reader.ReadInt32();
-		    for (int i = 0; i < count; i++)
+		    Pool.Load(reader);
+
+		    var items = 0;
+		    var factoryCount = reader.ReadInt32();
+		    for (int f = 0; f < factoryCount; f++)
 		    {
-			    var item = Facepunch.Pool.Get<Item>();
-			    item.identifier = reader.ReadUInt32();
-			    item.hash = reader.ReadBytes(reader.ReadInt32());
-			    item.encrypted = reader.ReadBoolean();
-			    ITEMS.Add(item.identifier, item);
+			    var factory = Facepunch.Pool.Get<Factory>();
+			    factory.id = reader.ReadUInt32();
+			    var itemCount = reader.ReadInt32();
+			    for (int i = 0; i < itemCount; i++)
+			    {
+				    items++;
+				    var item = Facepunch.Pool.Get<Item>();
+				    item.id = reader.ReadUInt32();
+				    item.hash = reader.ReadBytes(reader.ReadInt32());
+				    if (reader.ReadBoolean())
+				    {
+					    item.salt = reader.ReadBytes(reader.ReadInt32());
+				    }
+				    item.encrypted = reader.ReadBoolean();
+				    factory.AddItem(item);
+			    }
+			    FACTORIES.Add(factory);
 		    }
+
+		    if (!silent) Logger.Log($"Loaded Carbon.Vault with {factoryCount:n0} {factoryCount.Plural("factory", "factories")} and {items:n0} {items.Plural("item", "items")}");
 	    }
 	    catch (Exception ex)
 	    {
@@ -184,17 +269,75 @@ public class Vault
 	    }
     }
 
-    private class Item : Facepunch.Pool.IPooled
+    public class Factory : List<Item>, Facepunch.Pool.IPooled
     {
-	    public uint identifier;
-	    public byte[] hash;
+	    public uint id;
+	    public void EnterPool()
+	    {
+		    id = 0;
+		    foreach (var item in this)
+		    {
+			    var i = item;
+			    Facepunch.Pool.Free(ref i);
+		    }
+		    Clear();
+	    }
+	    public void LeavePool()
+	    {
+	    }
+
+	    public bool HasItem(uint item)
+	    {
+		    for (int i = 0; i < Count; i++)
+		    {
+			    var value = this[i];
+			    if(value.id == item) return true;
+		    }
+		    return false;
+	    }
+	    public bool HasItem(uint item, out Item value)
+	    {
+		    for (int i = 0; i < Count; i++)
+		    {
+			    value = this[i];
+			    if(value.id == item) return true;
+		    }
+		    value = null;
+		    return false;
+	    }
+	    public Item GetItem(uint item)
+	    {
+		    for (int i = 0; i < Count; i++)
+		    {
+			    var value = this[i];
+			    if(value.id == item) return value;
+		    }
+		    return null;
+	    }
+	    public void AddItem(Item item)
+	    {
+		    if (GetItem(item.id) != null) return;
+		    Add(item);
+	    }
+	    public bool RemoveItem(uint item)
+	    {
+		    return HasItem(item, out var value) && Remove(value);
+	    }
+    }
+
+    public class Item : Facepunch.Pool.IPooled
+    {
+	    public uint id;
 	    public bool encrypted;
-	    public string cache;
+	    internal string cache;
+	    internal byte[] salt;
+	    internal byte[] hash;
 
 	    public void EnterPool()
 	    {
-		    identifier = 0;
+		    id = 0;
 		    hash = null;
+		    salt = null;
 		    encrypted = false;
 		    cache = null;
 	    }
@@ -203,12 +346,32 @@ public class Vault
 	    }
     }
 
-    private static class Pool
+    public static class Pool
     {
 	    private static Dictionary<string, uint> NamePoolString = new();
 	    private static Dictionary<uint, string> NamePoolInt = new();
 
-	    public static uint GetOrAdd(string name)
+	    public static void Save(BinaryWriter writer)
+	    {
+		    writer.Write(NamePoolString.Count);
+		    foreach (var value in NamePoolString)
+		    {
+			    writer.Write(value.Key);
+			    writer.Write(value.Value);
+		    }
+	    }
+	    public static void Load(BinaryReader reader)
+	    {
+		    var count = reader.ReadInt32();
+		    for (int i = 0; i < count; i++)
+		    {
+			    var key = reader.ReadString();
+			    var value = reader.ReadUInt32();
+			    NamePoolString[key] = value;
+			    NamePoolInt[value] = key;
+		    }
+	    }
+	    public static uint Get(string name)
 	    {
 		    if (NamePoolString.TryGetValue(name, out var hash))
 		    {
@@ -219,6 +382,11 @@ public class Vault
 		    NamePoolString[name] = hash;
 		    NamePoolInt[hash] = name;
 		    return hash;
+	    }
+
+	    public static string Get(uint hash)
+	    {
+		    return NamePoolInt.TryGetValue(hash, out var name) ? name : null;
 	    }
 	    private static uint ManifestHash(string str)
 	    {
