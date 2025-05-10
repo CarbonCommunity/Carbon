0213945713ba2e5a644495c47abb34d492b1e7ea
raul <raulssorban@gmail.com>
2025-05-10 05:19:41 +0200

    I lied, messages overhaul

--- src/Carbon/Components/Bridge.cs
+++ src/Carbon/Components/Bridge.cs
@@ -10,11 +10,53 @@ public static class Bridge
 {
 	public static BridgeServer Server = new();
 
-	public static async ValueTask<BridgeClient> StartClient(string ip, int port, string password, Action<BridgeRead> onRead, int maxBufferSize = 8192)
+	public static async ValueTask<BridgeClient> StartClient(string ip, int port, string password, BridgeMessages messages, int maxBufferSize = 8192)
 	{
-		var client = new BridgeClient();
-		await client.Connect(ip, port, password, onRead, maxBufferSize);
-		return client;
+		return await new BridgeClient().Connect(ip, port, password, messages, maxBufferSize);
+	}
+}
+
+public class BridgeMessages : Pool.IPooled
+{
+	public Action<BridgeRead> OnRpc;
+	public Action<BridgeRead> OnCommand;
+	public Action<BridgeRead> OnCustom;
+
+	public static BridgeMessages Rent() => Pool.Get<BridgeMessages>();
+
+	public static void Return(ref BridgeMessages messages) => Pool.Free(ref messages);
+
+	public void HandleRead(BridgeRead read)
+	{
+		switch (read.BridgeMessage())
+		{
+			case Types.Rpc:
+				OnRpc?.Invoke(read);
+				break;
+			case Types.Command:
+				OnCommand?.Invoke(read);
+				break;
+			case Types.Custom:
+				OnCustom?.Invoke(read);
+				break;
+		}
+	}
+
+	public enum Types
+	{
+		Rpc,
+		Command,
+		Custom
+	}
+
+	public void EnterPool()
+	{
+		OnRpc = null;
+		OnCommand = null;
+		OnCustom = null;
+	}
+	public void LeavePool()
+	{
 	}
 }
 
@@ -23,11 +65,12 @@ public class BridgeServer
 	public Listener Listener;
 	public Action<BridgeConnection> OnNewConnection;
 	public Action<BridgeConnection> OnClosedConnection;
+	public BridgeMessages Messages = new();
 	public readonly Dictionary<int, BridgeConnection> Connections = [];
 
 	public void Start(int port, string password, string ip = null)
 	{
-		if (password is null or "unset")
+		if (password is null or "unset" or "password")
 		{
 			return;
 		}
@@ -52,7 +95,7 @@ public class BridgeServer
 				else
 				{
 					var connectionId = Interlocked.Increment(ref Listener.nextClientId);
-					var bridgeConnection = Pool.Get<BridgeConnection>().Init(socket);
+					var bridgeConnection = Pool.Get<BridgeConnection>().Init(socket, Messages);
 					socket.OnOpen = () =>
 					{
 						lock (Listener.clients)
@@ -88,9 +131,8 @@ public class BridgeServer
 						{
 							stream._buffer[i] = data[i];
 						}
-
 						var read = BridgeRead.Rent(bridgeConnection, stream);
-						bridgeConnection.OnRead(read);
+						Messages.HandleRead(read);
 						BridgeRead.Return(ref read);
 					};
 					socket.OnError = e =>
@@ -108,13 +150,13 @@ public class BridgeClient
 {
 	public ClientWebSocket Socket;
 	public CancellationTokenSource CancellationToken;
-	public Action<BridgeRead> OnRead;
+	public BridgeMessages Messages;
 	public int MaxBufferSize;
 
-	public async ValueTask Connect(string ip, int port, string password, Action<BridgeRead> onRead, int maxBufferSize = 8192)
+	public async ValueTask<BridgeClient> Connect(string ip, int port, string password, BridgeMessages messages, int maxBufferSize = 8192)
 	{
 		MaxBufferSize = maxBufferSize;
-		OnRead = onRead;
+		Messages = messages;
 		Socket = new ClientWebSocket();
 		CancellationToken = new CancellationTokenSource();
 
@@ -128,6 +170,8 @@ public class BridgeClient
 		{
 			Logger.Error($"Carbon.Bridge Client connection attempt to '{ip}:{port}' failed", ex);
 		}
+
+		return this;
 	}
 	public async ValueTask Disconnect()
 	{
@@ -137,7 +181,10 @@ public class BridgeClient
 		CancellationToken = null;
 		Socket.Dispose();
 		Socket = null;
-		OnRead = null;
+		if (Messages != null)
+		{
+			BridgeMessages.Return(ref Messages);
+		}
 	}
 
 	private async ValueTask ReceiveLoop()
@@ -162,7 +209,7 @@ public class BridgeClient
 						read.stream = stream;
 						try
 						{
-							OnRead?.Invoke(read);
+							Messages.HandleRead(read);
 						}
 						catch (Exception ex)
 						{
@@ -187,12 +234,12 @@ public class BridgeClient
 public class BridgeConnection : Pool.IPooled
 {
 	public IWebSocketConnection Socket;
-	public Action<BridgeRead> OnRead;
+	public BridgeMessages Messages;
 
-	public BridgeConnection Init(IWebSocketConnection connection)
+	public BridgeConnection Init(IWebSocketConnection connection, BridgeMessages messages)
 	{
 		this.Socket = connection;
-		this.OnRead = OnRead;
+		this.Messages = messages;
 		return this;
 	}
 
@@ -203,7 +250,7 @@ public class BridgeConnection : Pool.IPooled
 
 	public void EnterPool()
 	{
-		OnRead = null;
+		Messages = null;
 		Socket = null;
 	}
 
@@ -211,12 +258,6 @@ public class BridgeConnection : Pool.IPooled
 	{
 
 	}
-}
-
-public enum BridgeMessages
-{
-	Handshake,
-	Rpc
 }
 
 public sealed class BridgeRead : NetRead
@@ -242,9 +283,9 @@ public sealed class BridgeRead : NetRead
 		this.bridgeConnection = conn;
 	}
 
-	public BridgeMessages PeekBridgeMessage() => Peek<BridgeMessages>();
+	public BridgeMessages.Types PeekBridgeMessage() => Peek<BridgeMessages.Types>();
 
-	public BridgeMessages BridgeMessage() => (BridgeMessages)Int32();
+	public BridgeMessages.Types BridgeMessage() => (BridgeMessages.Types)Int32();
 
 	public new void EnterPool()
 	{
@@ -273,7 +314,7 @@ public sealed class BridgeWrite : NetWrite
 		return new MemoryBuffer(buffer.Buffer, buffer.Length, fromPool);
 	}
 
-	public void BridgeMessage(BridgeMessages message)
+	public void BridgeMessage(BridgeMessages.Types message)
 	{
 		Int32((int)message);
 	}
