34071bfbbecd8082b406eb42fe6d7304987208bd
raul <raulssorban@gmail.com>
2025-06-18 23:05:53 +0200

    Command rework

--- src/Carbon/CorePlugin/Core.Commands.Test.cs
+++ src/Carbon/CorePlugin/Core.Commands.Test.cs
@@ -6,22 +6,29 @@ public partial class CorePlugin
 {
 	[ConsoleCommand("test_beds", "Prints all currently queued up tests ready to be executed.")]
 	[AuthLevel(2)]
-	private void TestBeds(ConsoleSystem.Arg arg)
+	private void test_beds(ConsoleSystem.Arg arg)
 	{
-		using var table = new StringTable(string.Empty, "context", "tests");
+		using var table = new StringTable(string.Empty, "context", "tests", "channel");
 
-		foreach (var bed in Integrations.Banks)
+		foreach (var bed in Integrations.Banks.Values.SelectMany(bank => bank))
 		{
-			table.AddRow(string.Empty, bed.Context, $"{bed.Count:n0}");
+			table.AddRow(string.Empty, bed.Context, $"{bed.Count:n0}", bed.Channel.ToString());
 		}
 
 		arg.ReplyWith(table.ToStringMinimal());
 	}
 
-	[ConsoleCommand("test_run", "Executes all Test Beds that are currently queued up.")]
+	[ConsoleCommand("test_run", "Executes all Test Beds that are currently queued up. Eg. c.test_run <channel|-1> [<delay|0.1>]")]
 	[AuthLevel(2)]
-	private void TestRun(ConsoleSystem.Arg arg)
+	private void test_run(ConsoleSystem.Arg arg)
 	{
+		if (!arg.HasArgs(1))
+		{
+			arg.ReplyWith($"Syntax: c.test_run <channel|-1> [<delay|0.1>]");
+			return;
+		}
+
+		var channel = arg.GetInt(0, -1);
 		var delay = arg.GetFloat(0, 0.1f);
 
 		if (delay < 0)
@@ -30,13 +37,13 @@ public partial class CorePlugin
 			return;
 		}
 
-		Integrations.Run(delay);
+		Integrations.Run(delay, channel);
 	}
 
-	[ConsoleCommand("test_clear", "Clears all Test Beds that are currently queued up.")]
+	[ConsoleCommand("test_clear", "Clears all Test Beds that are currently queued up. Eg. c.test_clear [<channel|-1>]")]
 	[AuthLevel(2)]
-	private void TestClear(ConsoleSystem.Arg arg)
+	private void test_clear(ConsoleSystem.Arg arg)
 	{
-		Integrations.Clear();
+		Integrations.Clear(arg.GetInt(0, -1));
 	}
 }
