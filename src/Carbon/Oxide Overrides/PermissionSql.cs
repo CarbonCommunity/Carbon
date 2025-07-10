290ea386f435e3278198e7e757e9630861664d58
raul <raulssorban@gmail.com>
2025-07-10 23:53:19 +0200

    Added CreateGroup, RemoveGroup and SetGroupTitle SQL implementation Added proper DB disposal and closure

--- src/Carbon/Oxide Overrides/PermissionSql.cs
+++ src/Carbon/Oxide Overrides/PermissionSql.cs
@@ -13,16 +13,70 @@ public class PermissionSql : Permission
 		var path = Path.Combine(ConVar.Server.filesStorageFolder, "carbon.perms.db");
 		db = new PermissionDatabase();
 		db.Open(path);
-		if (db.TableExists("data"))
+		if (db.TableExists("users"))
 		{
 			return;
 		}
-		
+		this.db.Execute("CREATE TABLE users ( userId INTEGER PRIMARY KEY, lastSeenNickname TEXT, language TEXT )");
+		this.db.Execute("CREATE TABLE groups ( groupName TEXT PRIMARY KEY, title TEXT, rank INTEGER, parentGroup TEXT )");
+		this.db.Execute("CREATE INDEX IF NOT EXISTS userindex ON users ( userId )");
+		this.db.Execute("CREATE INDEX IF NOT EXISTS groupindex ON groups ( groupName )");
+	}
+
+	public override bool CreateGroup(string group, string title, int rank)
+	{
+		if (base.CreateGroup(group, title, rank))
+		{
+			if (!group.IsLower())
+			{
+				group = group.ToLower();
+			}
+			db?.Execute("INSERT OR REPLACE INTO groups ( groupName, title, rank ) VALUES ( ?, ?, ? )", group, title, rank);
+			return true;
+		}
+
+		return false;
+	}
+
+	public override bool RemoveGroup(string group)
+	{
+		if (base.RemoveGroup(group))
+		{
+			if (!group.IsLower())
+			{
+				group = group.ToLower();
+			}
+			db?.Execute("DELETE FROM groups WHERE groupName = ?", group);
+			return true;
+		}
+		return false;
+	}
+
+	public override bool SetGroupTitle(string group, string title)
+	{
+		if (base.SetGroupTitle(group, title))
+		{
+			if (!group.IsLower())
+			{
+				group = group.ToLower();
+			}
+			db?.Execute("UPDATE groups SET title = ? WHERE groupName = ?", title, group);
+			return true;
+		}
+		return false;
+	}
+
+	public override void Dispose()
+	{
+		base.Dispose();
+		db.Close();
+		db = null;
 	}
 
 	public override void LoadFromDatafile()
 	{
 		InitializeDb();
+		IsLoaded = true;
 	}
 
 	public class PermissionDatabase : Facepunch.Sqlite.Database
