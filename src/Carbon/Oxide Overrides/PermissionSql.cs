4f504cd21235584a9b5b6795ff4384d07dc6ad6d
raul <raulssorban@gmail.com>
2025-07-17 15:14:42 +0200

    Added default SQL database group initialization

--- src/Carbon/Oxide Overrides/PermissionSql.cs
+++ src/Carbon/Oxide Overrides/PermissionSql.cs
@@ -439,6 +439,18 @@ public class PermissionSql : Permission
 	{
 		InitializeDb();
 		LoadGroups();
+
+		var playerDefaultGroup = Community.Runtime.Config.Permissions.PlayerDefaultGroup;
+		var adminDefaultGroup = Community.Runtime.Config.Permissions.AdminDefaultGroup;
+		var moderatorDefaultGroup = Community.Runtime.Config.Permissions.ModeratorDefaultGroup;
+
+		if (!string.IsNullOrEmpty(playerDefaultGroup) && !GroupExists(playerDefaultGroup))
+			CreateGroup(playerDefaultGroup, playerDefaultGroup.ToCamelCase(), 0);
+		if (!string.IsNullOrEmpty(adminDefaultGroup) && !GroupExists(adminDefaultGroup))
+			CreateGroup(adminDefaultGroup, adminDefaultGroup.ToCamelCase(), 1);
+		if (!string.IsNullOrEmpty(moderatorDefaultGroup) && !GroupExists(moderatorDefaultGroup))
+			CreateGroup(moderatorDefaultGroup, moderatorDefaultGroup.ToCamelCase(), 1);
+
 		IsLoaded = true;
 	}
 
