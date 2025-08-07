c5083f4bb414946a2c78d05f9dc6a08d27c3f8ad
raul <raulssorban@gmail.com>
2025-08-07 03:58:21 +0200

    Submodules++

--- Carbon.Core/Carbon/src/Loaders/ScriptLoader.cs
+++ Carbon.Core/Carbon/src/Loaders/ScriptLoader.cs
@@ -77,10 +77,11 @@ public class ScriptLoader : IScriptLoader
 		ExecuteProcess(Community.Runtime.ZipDevScriptProcessor, true, except, ref count, zipDevPlugins);
 #endif
 
-		if(count == 0)
+		if (count == 0)
 		{
 			ModLoader.IsBatchComplete = true;
 			Community.Runtime.Events.Trigger(CarbonEvent.AllPluginsLoaded, EventArgs.Empty);
+			Community.Runtime.Events.Trigger(CarbonEvent.AllPluginsInitialized, EventArgs.Empty);
 		}
 
 		static void ExecuteProcess(IScriptProcessor processor, bool folderMode, IEnumerable<string> except, ref int count, params string[][] folders)
