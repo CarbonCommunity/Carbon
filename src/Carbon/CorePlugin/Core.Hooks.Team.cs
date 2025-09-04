a21ac49f981989e86704764b8497f571c96da59f
raul <raulssorban@gmail.com>
2025-09-04 16:22:14 +0200

    Added IOnTeamInvite (doesn't do anything, but Oxide added it as it'll be replaced when OnTeamMemberInvite gets deprecated)

---
+++ src/Carbon/CorePlugin/Core.Hooks.Team.cs
@@ -0,0 +1,12 @@
+﻿namespace Carbon.Core;
+
+#pragma warning disable IDE0051
+
+public partial class CorePlugin
+{
+	internal static object IOnTeamInvite(BasePlayer sender, BasePlayer receiver)
+	{
+		// Fire OnTeamInvite when the old OnTeamMemberInvite becomes deprecated
+		return null;
+	}
+}
