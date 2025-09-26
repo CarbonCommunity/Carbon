5117bf0f5488902dac39cd842625a487acb138e6
raul <raulssorban@gmail.com>
2025-09-27 00:25:04 +0200

    Moved that over there

--- src/Carbon/WebControlPanel/WebControlPanel.Rpc.Players.cs
+++ src/Carbon/WebControlPanel/WebControlPanel.Rpc.Players.cs
@@ -14,4 +14,34 @@ public static partial class WebControlPanel
 		}
 		SendRpcResponse(read.Connection, write);
 	}
+
+	public struct PlayerInfo(BasePlayer player)
+	{
+		private ulong steamId = player.userID;
+		private ulong ownerSteamId = player.OwnerID;
+		private string displayName = player.displayName;
+		private int ping = player.IsConnected ? Network.Net.sv.GetAveragePing(player.Connection) : -1;
+		private string address = player.Connection.ipaddress;
+		private ulong entityId = player.net.ID.Value;
+		private int connectedSeconds = player.secondsConnected;
+		private float violationLevel = player.violationLevel;
+		private int currentLevel = 0;
+		private int unspentXp = 0;
+		private float health = player.health;
+
+		public void Serialize(BridgeWrite write)
+		{
+			write.WriteObject(steamId);
+			write.WriteObject(ownerSteamId);
+			write.WriteObject(displayName);
+			write.WriteObject(ping);
+			write.WriteObject(address);
+			write.WriteObject(entityId);
+			write.WriteObject(connectedSeconds);
+			write.WriteObject(violationLevel);
+			write.WriteObject(currentLevel);
+			write.WriteObject(unspentXp);
+			write.WriteObject(health);
+		}
+	}
 }
