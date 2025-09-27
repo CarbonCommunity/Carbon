72a94572e0c4a08e5d6ddca33e6761200f6a1fc8
raul <raulssorban@gmail.com>
2025-09-27 13:31:37 +0200

    Added ConsoleTail RPC implementation

---
+++ src/Carbon/WebControlPanel/WebControlPanel.Rpc.Console.cs
@@ -0,0 +1,22 @@
+﻿using Facepunch;
+
+namespace Carbon;
+
+public static partial class WebControlPanel
+{
+	[WebCall]
+	[WebCall.Condition.Permission(PermissionTypes.ConsoleView)]
+	private static void RPC_ConsoleTail(BridgeRead read)
+	{
+		var logs = Output.HistoryOutput.Skip(read.Int32());
+		var write = StartRpcResponse();
+		write.WriteObject(logs.Count());
+		foreach (var log in logs)
+		{
+			write.WriteObject(log.Message);
+			write.WriteObject(log.Type);
+			write.WriteObject(log.Time);
+		}
+		SendRpcResponse(read.Connection, write);
+	}
+}
