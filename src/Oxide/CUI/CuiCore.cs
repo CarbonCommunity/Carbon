56330a3bfe1bc9d438f2327c78929973d3f6ca28
beee <mbedda@gmail.com>
2025-10-09 22:59:00 +0300

    Fix for RPCError when using CuiContentSizeFitterComponent
    Added `StringEnumConverter` for `CuiContentSizeFitterComponent.HorizontalFit` and `CuiContentSizeFitterComponent.VerticalFit`

--- src/Oxide/CUI/CuiCore.cs
+++ src/Oxide/CUI/CuiCore.cs
@@ -694,9 +694,11 @@ public class CuiContentSizeFitterComponent : ICuiComponent, ICuiEnableable
 {
 	public string Type => "UnityEngine.UI.ContentSizeFitter";
 
+	[JsonConverter(typeof(StringEnumConverter))]
 	[JsonProperty("horizontalFit")]
 	public ContentSizeFitter.FitMode HorizontalFit { get; set; }
 
+	[JsonConverter(typeof(StringEnumConverter))]
 	[JsonProperty("verticalFit")]
 	public ContentSizeFitter.FitMode VerticalFit { get; set; }
 
