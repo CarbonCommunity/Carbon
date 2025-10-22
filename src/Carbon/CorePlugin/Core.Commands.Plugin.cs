f9632f02d3b5377b52b26605b36ab10248fc7f65
raul <raulssorban@gmail.com>
2025-10-22 13:08:16 +0200

    Fixed `c.reload pluginA pluginB pluginC` not looking for all arguments (applies the same for `c.load` and `c.unload`)

--- src/Carbon/CorePlugin/Core.Commands.Plugin.cs
+++ src/Carbon/CorePlugin/Core.Commands.Plugin.cs
@@ -143,7 +143,7 @@ public partial class CorePlugin
 
 		ProcessableFilesLookup();
 
-		var name = arg.GetString(0);
+		var name = string.Join(" ", arg.Args);
 		switch (name)
 		{
 			case "*":
@@ -297,7 +297,7 @@ public partial class CorePlugin
 
 		ProcessableFilesLookup();
 
-		var name = arg.GetString(0);
+		var name = string.Join(" ", arg.Args);
 		switch (name)
 		{
 			case "*":
@@ -366,7 +366,7 @@ public partial class CorePlugin
 
 		ProcessableFilesLookup();
 
-		var name = arg.GetString(0);
+		var name = string.Join(" ", arg.Args);
 		switch (name)
 		{
 			case "*":
