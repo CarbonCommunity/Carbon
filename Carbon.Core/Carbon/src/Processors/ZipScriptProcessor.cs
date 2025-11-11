5a003c567db406bddbb4252be43c6e3b3ac9fb24
raul <raulssorban@gmail.com>
2025-11-11 17:06:36 +0100

    Timer reversal

--- Carbon.Core/Carbon/src/Processors/ZipScriptProcessor.cs
+++ Carbon.Core/Carbon/src/Processors/ZipScriptProcessor.cs
@@ -167,8 +167,10 @@ public class ZipScriptProcessor : BaseProcessor, IZipScriptProcessor
 					{
 						Logger.Warn($" Warning! '{Path.GetFileNameWithoutExtension(file)}' uses UnityEngine.GameObject.FindObjectsOfType. That may cause significant performance drops, and/or server stalls. Report to the developer or use at your own discretion!");
 					}
+
+					output = input.Replace("PluginTimers", "Timers");
 				}
-				finally
+				catch
 				{
 					output = input;
 				}
