81168162522dd685859ba51e0e85a68a45231e3e
raul <raulssorban@gmail.com>
2025-11-15 01:45:42 +0100

    Added `c.moddedvars` which prints a list of all user-modified Rust ConVars

--- src/Carbon/CorePlugin/Core.Commands.Command.cs
+++ src/Carbon/CorePlugin/Core.Commands.Command.cs
@@ -1,4 +1,5 @@
 ﻿using API.Commands;
+using Facepunch;
 
 namespace Carbon.Core;
 
@@ -58,6 +59,31 @@ public partial class CorePlugin
 			if (command.HasFlag(CommandFlags.Hidden) || (!string.IsNullOrEmpty(filter) && !command.Name.Contains(filter))) continue;
 
 			body.AddRow($" {command.Name}", command.Help);
+		}
+
+		arg.ReplyWith(body.Write(StringTable.FormatTypes.None));
+	}
+
+	[ConsoleCommand("moddedvars", "Prints a table/list of all modified Rust ConVars.")]
+	[AuthLevel(2)]
+	private void ModdedRustConVars(ConsoleSystem.Arg arg)
+	{
+		using var body = new StringTable("variable", "value", "original_value");
+		var filter = arg.Args != null && arg.Args.Length > 0 ? arg.GetString(0) : null;
+
+		foreach (var command in ConVarSnapshots.Snapshots)
+		{
+			if ((!string.IsNullOrEmpty(filter) && !command.Key.Contains(filter)))
+			{
+				continue;
+			}
+
+			var currentValue = command.Value.Field.Key.GetValue(null)?.ToString();
+			var originalValue = command.Value.Value?.ToString();
+			if (originalValue != currentValue && !string.IsNullOrEmpty(originalValue) && !string.IsNullOrEmpty(currentValue))
+			{
+				body.AddRow($" {command.Key}", currentValue, originalValue);
+			}
 		}
 
 		arg.ReplyWith(body.Write(StringTable.FormatTypes.None));
