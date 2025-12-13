13655548baf618dfe931f5cbc4281c410c2970b5
evs.ptr <evs.ptr@proton.me>
2025-12-13 23:47:19 +0500

    `update.cs` prebuild used project to avoid threading issues

--- Tools/Build/runners/update.cs
+++ Tools/Build/runners/update.cs
@@ -2,6 +2,10 @@ var branch = GetArg(1, "release");
 
 Warn($"Branch: {branch}");
 
+DotNet.Run("build", PathEnquotes(Home, "Tools", "DepotDownloader", "DepotDownloader"));
+DotNet.Run("build", PathEnquotes(Home, "Carbon.Core", "Carbon.Tools", "Carbon.Publicizer"));
+DotNet.Run("build", PathEnquotes(Home, "Carbon.Core", "Carbon.Tools", "Carbon.Generator"));
+
 System.Threading.Tasks.Task.WaitAll(
     System.Threading.Tasks.Task.Run(() => DownloadRustFiles("windows")),
     System.Threading.Tasks.Task.Run(() => DownloadRustFiles("linux"))
@@ -10,7 +14,7 @@ System.Threading.Tasks.Task.WaitAll(
 void DownloadRustFiles(string platform)
 {
 	Log($"Downloading {platform} Rust files..");
-	DotNet.Run("run", "--project", PathEnquotes(Home, "Tools", "DepotDownloader", "DepotDownloader"), 
+	DotNet.Run("run", "--no-build", "--project", PathEnquotes(Home, "Tools", "DepotDownloader", "DepotDownloader"),
 		"-os", platform, 
 		"-validate", 
 		"-app 258550",
@@ -22,11 +26,11 @@ void DownloadRustFiles(string platform)
 	Files.Create(Path(Home, "Rust", platform, "RustDedicated_Data", "Managed", ".hash"), hash);
 	Log($"Assembly-CSharp = {hash} [hash]");
 
-	DotNet.Run("run", "--project", PathEnquotes(Home, "Carbon.Core", "Carbon.Tools", "Carbon.Publicizer"), 
+	DotNet.Run("run", "--no-build", "--project", PathEnquotes(Home, "Carbon.Core", "Carbon.Tools", "Carbon.Publicizer"), 
 		PathEnquotes(Home, "Rust", platform, "RustDedicated_Data", "Managed"));
 }
 
-DotNet.Run("run", "--project", PathEnquotes(Home, "Carbon.Core", "Carbon.Tools", "Carbon.Generator"),
+DotNet.Run("run", "--no-build", "--project", PathEnquotes(Home, "Carbon.Core", "Carbon.Tools", "Carbon.Generator"),
 				  "--plugininput", PathEnquotes(Home, "Carbon.Core", "Carbon.Components", "Carbon.Common", "src", "Carbon", "CorePlugin"),
 				  "--rust", PathEnquotes(Home, "Rust", "windows", "RustDedicated_Data", "Managed"));
 
@@ -35,7 +39,7 @@ modules.AddRange(Directories.Get(Path(Home, "Carbon.Core", "Carbon.Components", 
 modules.AddRange(Directories.Get(Path(Home, "Carbon.Core", "Carbon.Components", "Carbon.Modules", "src")));
 
 var modulePaths = string.Join(";", modules);
-DotNet.Run("run", "--project", PathEnquotes(Home, "Carbon.Core", "Carbon.Tools", "Carbon.Generator"),
+DotNet.Run("run", "--no-build", "--project", PathEnquotes(Home, "Carbon.Core", "Carbon.Tools", "Carbon.Generator"),
 	"--plugininput", $"\"{modulePaths}\"",
 	"--pluginnamespace", "Carbon.Modules",
 	"--basename", "module",
