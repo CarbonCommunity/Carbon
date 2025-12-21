c0ba1ede12d8123f332c335bf414d0ddc9b71330
Raul-Sorin Sorban <raulssorban@users.noreply.github.com>
2025-12-21 15:25:31 +0000

    WIPlash

--- Tools/Build/runners/git_feedback.cs
+++ Tools/Build/runners/git_feedback.cs
@@ -1,11 +1,23 @@
 var home = Home;
 
+var tasks = new System.Collections.Generic.List<Task>();
+var repositoryCommits = new System.Collections.Generic.Dictionary<string, System.Collections.Generic.List<(
+	string commit, 
+	string date,
+	string author,
+	string message
+)>>();
 using var stream = new System.IO.MemoryStream();
 {
 	using var writer = new System.IO.BinaryWriter(stream);
 	{
-		//ProcessRepository(Path(home));
+		Log("Starting");
+
 		ProcessRepository(Path(home, "Carbon.Core", "Carbon.Components", "Carbon.Common"));
+
+		Task.WaitAll(tasks);
+
+		Log("Done");
 
 		System.IO.File.WriteAllBytes("git_out.dat", stream.ToArray());
 	}
@@ -18,28 +30,27 @@ void ProcessRepository(string path)
 	Log(path);
 	Log(string.Empty);
 
+	Git.SetQuiet(true);
 	Git.Run("fetch", "--all", "--prune");
 
 	var origin = Git.RunOutput("remote", "get-url", "--push", "--all", "origin");
-	var output = Git.RunOutput("log", "--all", "--date=iso-strict", "--format=%H%x1f%ad%x1f%S%x1f%s");
+	var output = Git.RunOutput("log", "--all", "--date=iso-strict", "--format=%H%x1f%ad%x1f%an%x1f%s");
 	var lines = output.Split('\n', StringSplitOptions.RemoveEmptyEntries);
 
 	for(int i = 0; i < lines.Length; i++)
 	{
 		var parts = lines[i].Split('\x1f');
-		if (parts.Length >= 4)
+		tasks.Add(Task.Run(TaskCallback));
+		void TaskCallback()
 		{
-			var commit = parts[0];
-			var date = parts[1];
-			var branch = parts[2].Replace("refs/heads/", string.Empty).Replace("refs/remotes/origin/", string.Empty);
-			var subject = parts[3];
-			if (branch.Contains("refs/tags"))
+			if (parts.Length >= 4)
 			{
-				continue;
+				var commit = parts[0];
+				var date = parts[1];
+				var author = parts[2];
+				var subject = parts[3];
+				Log($"{commit} | {author} | {date} | {subject}");
 			}
-			Log($"{commit} | {branch} | {date} | {subject}");
 		}
 	}
 }
-
-Log(Home);
