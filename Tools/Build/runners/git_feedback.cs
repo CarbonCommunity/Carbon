de95fad77787e37e5ddf67102f7931e29cbbd338
raul <raulssorban@gmail.com>
2025-12-21 17:21:30 +0100

    Finalized

--- Tools/Build/runners/git_feedback.cs
+++ Tools/Build/runners/git_feedback.cs
@@ -1,56 +1,80 @@
 var home = Home;
 
+Git.SetQuiet(true);
+
 var tasks = new System.Collections.Generic.List<Task>();
-var repositoryCommits = new System.Collections.Generic.Dictionary<string, System.Collections.Generic.List<(
-	string commit, 
-	string date,
-	string author,
-	string message
-)>>();
+var commits = new System.Collections.Generic.List<(
+    string repo,
+    string sha, 
+    string date,
+    string author,
+    string message
+    )>();
 using var stream = new System.IO.MemoryStream();
 {
-	using var writer = new System.IO.BinaryWriter(stream);
-	{
-		Log("Starting");
+    using var writer = new System.IO.BinaryWriter(stream);
+    {
+        ProcessRepository("Carbon", Path(home));
+        ProcessRepository("Carbon.Bootstrap", Path(home, "Carbon.Core", "Carbon.Components", "Carbon.Bootstrap"));
+        ProcessRepository("Carbon.Common", Path(home, "Carbon.Core", "Carbon.Components", "Carbon.Common"));
+        ProcessRepository("Carbon.Compat", Path(home, "Carbon.Core", "Carbon.Components", "Carbon.Compat"));
+        ProcessRepository("Carbon.Modules", Path(home, "Carbon.Core", "Carbon.Components", "Carbon.Modules"));
+        ProcessRepository("Carbon.Preloader", Path(home, "Carbon.Core", "Carbon.Components", "Carbon.Preloader"));
+        ProcessRepository("Carbon.SDK", Path(home, "Carbon.Core", "Carbon.Components", "Carbon.SDK"));
+        ProcessRepository("Carbon.Startup", Path(home, "Carbon.Core", "Carbon.Components", "Carbon.Startup"));
+        ProcessRepository("Carbon.Test", Path(home, "Carbon.Core", "Carbon.Components", "Carbon.Test"));
+        ProcessRepository("Carbon.Native", Path(home, "Carbon.Core", "Carbon.Native"));
+        ProcessRepository("Carbon.Profiler", Path(home, "Carbon.Core", "Carbon.Profiler"));
+        ProcessRepository("Carbon.UniTask", Path(home, "Carbon.Core", "Carbon.UniTask"));
+        ProcessRepository("Carbon.Hooks.Base", Path(home, "Carbon.Core", "Carbon.Hooks", "Carbon.Hooks.Base"));
+        ProcessRepository("Carbon.Hooks.Community", Path(home, "Carbon.Core", "Carbon.Hooks", "Carbon.Hooks.Community"));
 
-		ProcessRepository(Path(home, "Carbon.Core", "Carbon.Components", "Carbon.Common"));
-
-		Task.WaitAll(tasks);
-
-		Log("Done");
-
-		System.IO.File.WriteAllBytes("git_out.dat", stream.ToArray());
-	}
+        Task.WaitAll(tasks);
+        
+        writer.Write(commits.Count);
+        for (int i = 0; i < commits.Count; i++)
+        {
+            var commit = commits[i];
+            writer.Write(commit.repo);
+            writer.Write(commit.sha);
+            writer.Write(commit.date);
+            writer.Write(commit.author);
+            writer.Write(commit.message);
+        }
+        
+        System.IO.File.WriteAllBytes("git_out.dat", stream.ToArray());
+    }
 }
 
-void ProcessRepository(string path)
+void ProcessRepository(string repo, string workspace)
 {
-	SetHome(path);
+    tasks.Add(Task.Run(TaskProcess));
 
-	Log(path);
-	Log(string.Empty);
+    void TaskProcess()
+    {
+        var git = new Carbon.Runner.Executors.Git();
+        git.SetQuiet(true);
+        git.WorkingDirectory(workspace);
+        git.Run("fetch", "--all", "--prune");
 
-	Git.SetQuiet(true);
-	Git.Run("fetch", "--all", "--prune");
+        var origin = git.RunOutput("remote", "get-url", "--push", "--all", "origin");
+        var output = git.RunOutput("log", "--all", "--date=iso-strict", "--format=%H%x1f%ad%x1f%an%x1f%s");
 
-	var origin = Git.RunOutput("remote", "get-url", "--push", "--all", "origin");
-	var output = Git.RunOutput("log", "--all", "--date=iso-strict", "--format=%H%x1f%ad%x1f%an%x1f%s");
-	var lines = output.Split('\n', StringSplitOptions.RemoveEmptyEntries);
-
-	for(int i = 0; i < lines.Length; i++)
-	{
-		var parts = lines[i].Split('\x1f');
-		tasks.Add(Task.Run(TaskCallback));
-		void TaskCallback()
-		{
-			if (parts.Length >= 4)
-			{
-				var commit = parts[0];
-				var date = parts[1];
-				var author = parts[2];
-				var subject = parts[3];
-				Log($"{commit} | {author} | {date} | {subject}");
-			}
-		}
-	}
+        var lines = output.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        var count = 0;
+        for (int i = 0; i < lines.Length; i++)
+        {
+            var parts = lines[i].Split('\x1f');
+            if (parts.Length >= 4)
+            {
+                var sha = parts[0];
+                var date = parts[1];
+                var author = parts[2];
+                var message = parts[3];
+                commits.Add((repo, sha, date, author, message));
+                count++;
+            }
+        }
+        Log($"Collected {count} commits for {repo}");
+    }
 }
