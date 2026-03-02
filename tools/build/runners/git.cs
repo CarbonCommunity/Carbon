df38ffecaaa598e7315843929d74ef5d435ff93b
raul <raulssorban@gmail.com>
2026-03-02 12:30:00 +0100

    Cleanup git metadata outputs

--- tools/build/runners/git.cs
+++ tools/build/runners/git.cs
@@ -4,21 +4,21 @@ Warn($"Local Tag: {localTag}");
 var temp = Path(Home, "src", ".tmp");
 Directories.Create(temp);
 
-Files.Create(Path(temp, ".gitbranch"), Git.RunOutput("branch", "--show-current"));
-Files.Create(Path(temp, ".gitchs"), Git.RunOutput("rev-parse", "--short", "HEAD"));
-Files.Create(Path(temp, ".gitchl"), Git.RunOutput("rev-parse", "--long", "HEAD"));
-Files.Create(Path(temp, ".gitauthor"), Git.RunOutput("show", "-s", "--format=\"%an\"", "HEAD"));
-Files.Create(Path(temp, ".gitcomment"), Git.RunOutput("log -1", "--pretty=\"%B\"", "HEAD"));
-Files.Create(Path(temp, ".gitdate"), Git.RunOutput("log -1", "--format=\"%ci\"", "HEAD"));
+Files.Create(Path(temp, ".gitbranch"), Git.RunOutput("branch", "--show-current").Trim());
+Files.Create(Path(temp, ".gitchs"), Git.RunOutput("rev-parse", "--short", "HEAD").Trim());
+Files.Create(Path(temp, ".gitchl"), Git.RunOutput("rev-parse", "--long", "HEAD").Replace("--long", null).Trim());
+Files.Create(Path(temp, ".gitauthor"), Git.RunOutput("show", "-s", "--format=\"%an\"", "HEAD").Trim());
+Files.Create(Path(temp, ".gitcomment"), Git.RunOutput("log -1", "--pretty=\"%B\"", "HEAD").Trim());
+Files.Create(Path(temp, ".gitdate"), Git.RunOutput("log -1", "--format=\"%ci\"", "HEAD").Trim());
 
 if(string.IsNullOrEmpty(localTag))
 {
-    Files.Create(Path(temp, ".gittag"), Git.RunOutput("describe", "--tags"));
+    Files.Create(Path(temp, ".gittag"), Git.RunOutput("describe", "--tags").Trim());
 }
 else
 {
     Files.Create(Path(temp, ".gittag"), localTag);
 }
 
-Files.Create(Path(temp, ".giturl"), Git.RunOutput("remote", "get-url", "origin"));
-Files.Create(Path(temp, ".gitchanges"), Git.RunOutput("log -1", "--name-status", "--format=\"\""));
+Files.Create(Path(temp, ".giturl"), Git.RunOutput("remote", "get-url", "origin").Trim());
+Files.Create(Path(temp, ".gitchanges"), Git.RunOutput("log -1", "--name-status", "--format=\"\"").Trim());
