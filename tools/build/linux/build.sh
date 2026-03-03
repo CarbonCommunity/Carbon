2f0a3c52afe094864f1d6bd1cb6c4ac05a4d93f3
evs-ptr <evs.ptr@proton.me>
2026-03-03 17:42:28 +0500

    `*.sh` update paths & chmod +x

--- tools/build/linux/build.sh
+++ tools/build/linux/build.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 BASE="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 && pwd -P)"
 
-"${BASE}/_runner.sh" Tools/Build/runners/build.cs "$@"
+"${BASE}/_runner.sh" tools/build/runners/build.cs "$@"
