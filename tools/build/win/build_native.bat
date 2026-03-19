10bbde447ed647f31de434fc277b7e405f4ac50d
evs-ptr <evs.ptr@proton.me>
2026-03-19 17:40:52 +0500

    Sync `*.bat` build scripts to linux logic
    - Build scripts does not do `norestore` by default now to avoid failing on initial clean project, now norestore should be called via arg `./build_minimal_noarchive.bat -norestore`
    - Allow win build scripts to be called from project root path, like: `./tools/build/win/update_staging.bat`

--- tools/build/win/build_native.bat
+++ tools/build/win/build_native.bat
@@ -1,14 +1,18 @@
 @echo off
 
-set HOME=%cd%
-set ROOT=%HOME%\..\..\..
+for %%I in ("%~dp0..\..\..") do set "ROOT=%%~fI"
+set "NATIVE_DIR=%ROOT%\src\Carbon.Native"
 
-cd %ROOT%\src\Carbon.Native
+pushd "%NATIVE_DIR%" >nul || exit /b 1
 
-podman machine start
-rustup target add x86_64-unknown-linux-gnu
-rustup target add x86_64-pc-windows-gnu
-cross build -r --target x86_64-unknown-linux-gnu
+podman machine start || goto :done
+rustup target add x86_64-unknown-linux-gnu || goto :done
+rustup target add x86_64-pc-windows-gnu || goto :done
+cross build -r --target x86_64-unknown-linux-gnu || goto :done
 cross build -r --target x86_64-pc-windows-gnu
 
-cd %HOME%
+:done
+set "EXIT_CODE=%ERRORLEVEL%"
+popd >nul
+
+exit /b %EXIT_CODE%
