f7d5eb7caa9dd036adc8d5dca6e30c31e322293a
raul <raulssorban@gmail.com>
2026-06-13 14:39:11 +0200

    Use StringView version of the name when mapping protected strings

--- src/Carbon.Components/Carbon.Common/src/Community.Runtime.cs
+++ src/Carbon.Components/Carbon.Common/src/Community.Runtime.cs
@@ -83,10 +83,10 @@ public partial class Community
 		var str = new StringView(name);
 		var spaceIndex = str.IndexOf(' ');
 		var command = spaceIndex < 0 ? str : str.Substring(0, spaceIndex);
-		if (_protect.TryGetValue(name, out var cached))
+		if (_protect.TryGetValue(str, out var cached))
 			return cached.ToString();
 
 		var args = spaceIndex < 0 ? string.Empty : str.Substring(spaceIndex + 1);
-		return (_protect[name] = new StringView(Vault.Pool.Get(command + RuntimeId).ToString() + " " + args)).ToString();
+		return (_protect[str] = new StringView(Vault.Pool.Get(command + RuntimeId).ToString() + " " + args)).ToString();
 	}
 }
