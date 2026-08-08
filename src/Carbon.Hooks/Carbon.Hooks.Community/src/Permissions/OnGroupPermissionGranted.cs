827881b95c1d925032c2481783dc3c7d11c892bd
evs-ptr <evs.ptr@proton.me>
2026-08-09 01:25:51 +0500

    `Hooks.Community` mark the permission and group events as discarding their return value

--- src/Carbon.Hooks/Carbon.Hooks.Community/src/Permissions/OnGroupPermissionGranted.cs
+++ src/Carbon.Hooks/Carbon.Hooks.Community/src/Permissions/OnGroupPermissionGranted.cs
@@ -17,6 +17,7 @@ public partial class Category_Engine
 		[MetadataAttribute.Parameter("group", typeof(string))]
 		[MetadataAttribute.Parameter("permission", typeof(string))]
 		[MetadataAttribute.Assembly("Carbon.Common.dll")]
+		[MetadataAttribute.Return(Discarded = true)]
 		[MetadataAttribute.OxideCompatible]
 
 		public class OnGroupPermissionGranted : Patch;
