8cf34840ea15897c2697d13f86602e3ee10e2491
raul <raulssorban@gmail.com>
2026-08-25 18:50:15 +0200

    Added proxy SetFlag global extension so all plugins won't break

--- src/Carbon.Components/Carbon.Common/src/Carbon/Extensions/RustEx.cs
+++ src/Carbon.Components/Carbon.Common/src/Carbon/Extensions/RustEx.cs
@@ -1,7 +1,10 @@
-﻿using Facepunch;
-
-public static class RustEx
+﻿public static class RustEx
 {
 	public static void SendFullSnapshot(this BasePlayer player) => player.SendCompleteSnapshot();
 	public static void SendChangesToClient(this PlayerMetabolism metabolism) => metabolism.SendChanges();
+	public static void SetFlag(this BaseEntity entity, BaseEntity.Flags f, bool b, bool recursive = false, bool networkupdate = true)
+	{
+		using var flags = entity.StartSetFlags(networkupdate ? BaseEntity.FlagsUpdateMode.SendNetworkUpdate : BaseEntity.FlagsUpdateMode.Local);
+		flags.Set(f, b, recursive);
+	}
 }
