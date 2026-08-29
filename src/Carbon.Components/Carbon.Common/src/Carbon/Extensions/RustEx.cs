d1c8f258261ce1977cb0b89743c75734c73c7c30
raul <raulssorban@gmail.com>
2026-08-29 13:15:05 +0200

    Made all Carbon proxy methods obsolete

--- src/Carbon.Components/Carbon.Common/src/Carbon/Extensions/RustEx.cs
+++ src/Carbon.Components/Carbon.Common/src/Carbon/Extensions/RustEx.cs
@@ -1,7 +1,12 @@
 ﻿public static class RustEx
 {
+	[Obsolete("Use BasePlayer.SendCompleteSnapshot instead.")]
 	public static void SendFullSnapshot(this BasePlayer player) => player.SendCompleteSnapshot();
+
+	[Obsolete("Use PlayerMetabolism.SendChanges instead.")]
 	public static void SendChangesToClient(this PlayerMetabolism metabolism) => metabolism.SendChanges();
+
+	[Obsolete("Use BaseEntity.StartSetFlags or BaseEntity.SetFlagLocal instead.")]
 	public static void SetFlag(this BaseEntity entity, BaseEntity.Flags f, bool b, bool recursive = false, bool networkupdate = true)
 	{
 		using var flags = entity.StartSetFlags(networkupdate ? BaseEntity.FlagsUpdateMode.SendNetworkUpdate : BaseEntity.FlagsUpdateMode.Local);
